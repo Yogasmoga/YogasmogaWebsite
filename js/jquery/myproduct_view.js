f39a545a0784b328717de3c576407afe418cacca
ankit4mobikasa <ankit@mobikasa.com>
2012-11-17 18:58:45 +0530

    New Layout & Product Page
    Created New Layout for My Account Section and added description to
    design features

--- js/jquery/myproduct_view.js
+++ js/jquery/myproduct_view.js
@@ -3,6 +3,20 @@ jQuery(document).ready(function($){
     $(window).resize(function(){
         setTimeout(function(){ resizeDesignFeatures(); }, 100);    
     });
+    
+    $("table.productdesignfeatures div[size]").hover(function(){
+        $(this).find("div.caption").height();
+        //$(this).find("div.caption").css('margin-top', ($(this).find("div.caption").height() * -1) + 'px');
+        $(this).find("div.caption").animate({
+            'margin-top': ($(this).find("div.caption").height() * -1)
+        }, 300);
+    },
+    function(){
+        //$(this).find("div.caption").css('margin-top', '0px');
+        $(this).find("div.caption").animate({
+            'margin-top': 0
+        }, 300);
+    });
     //$("table.productdesignfeatures td div img").capty();
     //$("table.productdesignfeatures td div img").hover(function(){
 //       var div = $(this).parent().find("div.caption:first");
@@ -38,7 +52,8 @@ function resizeDesignFeatures()
         var parentheight = (jQuery(this).parent().css('height').substr(0, jQuery(this).parent().css('height').length - 2) * 1);
         if(height > parentheight)
         {
-            jQuery(this).css('margin-top',((parentheight - height) / 2) + 'px');        
+            //jQuery(this).css('margin-top',((parentheight - height) / 2) + 'px');
+            //jQuery(this).parent().css('margin-top',((parentheight - height) / 2) + 'px');        
         }
         //jQuery(this).css('height',parentheight + 'px');
         //var height = jQuery(this).width();
