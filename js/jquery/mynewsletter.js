4eaed9286dae703c39ffb868e0e79f4f2b0bd242
Ashish Bhagat <ashish@blankandco.com>
2013-01-03 15:35:44 +0530

    changes

--- js/jquery/mynewsletter.js
+++ js/jquery/mynewsletter.js
@@ -7,37 +7,47 @@ jQuery(document).ready(function($){
     });
     
     $("#btnNewsletter").click(function(){
-        SubscribeNewsletter();
+        SubscribeNewsletter('#txtNewsletterEmail','#footernotification');
     });
     
+    $("#txtNewsletterEmail1").keypress(function (event) {
+        if (event.which == 13) {
+            SubscribeNewsletter();
+            return false;
+        }
+    });
+    
+    $("#btnNewsletter1").click(function(){
+        SubscribeNewsletter('#txtNewsletterEmail1','#notification1');
+    });
 });
 
-function SubscribeNewsletter()
+function SubscribeNewsletter(input, notification)
 {
-    if(jQuery("#txtNewsletterEmail").val() == "" || jQuery("#txtNewsletterEmail").val() == jQuery("#txtNewsletterEmail").attr("watermark"))
+    if(jQuery(input).val() == "" || jQuery(input).val() == jQuery(input).attr("watermark"))
     {
-        jQuery("#footernotification").html("Please provide your Email Address").removeClass("success").addClass("error").fadeIn();
+        jQuery(notification).html("Please provide your Email Address").removeClass("success").addClass("error").fadeIn();
         return;
     }
-    if(!validateEmail(jQuery("#txtNewsletterEmail").val()))
+    if(!validateEmail(jQuery(input).val()))
     {
-        jQuery("#footernotification").html("Doesn't look like that's a valid Email Address").removeClass("success").addClass("error").fadeIn();
+        jQuery(notification).html("Doesn't look like that's a valid Email Address").removeClass("success").addClass("error").fadeIn();
         return;
     }
     else
     {
-        jQuery("#footernotification").hide();
+        jQuery(notification).hide();
     }
     jQuery.ajax({
         type : 'POST',
         url : homeUrl + 'mynewsletter/mysubscriber/add',
-        data : {'email': jQuery("#txtNewsletterEmail").val()},
+        data : {'email': jQuery(input).val()},
         success : function(result){
             result = eval('(' + result + ')');
             if(result.status == "0")
-                jQuery("#footernotification").html(result.message).removeClass("success").addClass("error").fadeIn();
+                jQuery(notification).html(result.message).removeClass("success").addClass("error").fadeIn();
             else
-                jQuery("#footernotification").html(result.message).removeClass("error").addClass("success").fadeIn();
+                jQuery(notification).html(result.message).removeClass("error").addClass("success").fadeIn();
             //setTimeout(function(){ rremovenotifications(); }, 5000);
         }
     });
