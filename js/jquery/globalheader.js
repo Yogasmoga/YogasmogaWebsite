fd2abafeda3758e35cb4bf666aa79a841908d18b
ankit4mobikasa <ankit@mobikasa.com>
2013-02-02 15:45:03 +0530

    bug

--- js/jquery/globalheader.js
+++ js/jquery/globalheader.js
@@ -1,4 +1,10 @@
 jQuery(document).ready(function($){
+    
+    if(_currenturl.indexOf('#') > 0)
+    {
+        $(window).scrollTop($(window).scrollTop() - 80);
+    }
+    
 	$(window).load(function(){
 			//$('.header-container').hover(
 	//        function() {
