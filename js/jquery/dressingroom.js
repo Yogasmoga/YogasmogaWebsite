19336207a1b17db84b4d3ebe9eaa5bd1106d5924
ankit4mobikasa <ankit@mobikasa.com>
2013-02-04 21:22:14 +0530

    dressing room

--- js/jquery/dressingroom.js
+++ js/jquery/dressingroom.js
@@ -158,7 +158,11 @@ function chkfixposition(){
 		$j(this).height(newHeight);
 	});
     //console.log(((jQuery(".fullscreen:first").width() / 2) - (jQuery("div#dressingroombottom div.dritem.active img").width() / 2) - 30) - jQuery("div."));
-    jQuery("div#drsizechart").css('left', ((jQuery(".fullscreen:first").width() / 2) - (jQuery("div#dressingroombottom div.dritem.active img").width() / 2) - 25) - (jQuery("div#drsizechart").width()) + 'px'); 
+    jQuery("div#drsizechart").css('left', ((jQuery(".fullscreen:first").width() / 2) - (jQuery("div#dressingroombottom div.dritem.active img").width() / 2) - 25) - (jQuery("div#drsizechart").width()) + 'px');
+    if(_onipad)
+    {
+        jQuery("#pageScrollerNav").css('top', jQuery("#dressingroomdivider").offset().top - (jQuery("#pageScrollerNav").height() / 2));
+    } 
 }
 function getScaledheight(originalheight, originalwidth)
 {
