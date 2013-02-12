35286c301571850ea32aceb0a20abf1065324080
ankit4mobikasa <ankit@mobikasa.com>
2013-02-12 18:54:31 +0530

    notification

---
+++ app/code/local/Mycustommodules/Myessentials/controllers/MylinkController.php
@@ -0,0 +1,9 @@
+<?php 
+class Mycustommodules_Myessentials_MylinkController extends Mage_Core_Controller_Front_Action
+{
+    public function testAction()
+    {
+        echo "Output from link Module";
+    }
+}
+?>
