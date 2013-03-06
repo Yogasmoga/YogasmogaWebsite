8c4513f3a77e792a0e673b1f22c1399127bbaafb
Ashish Bhagat <ashish@blankandco.com>
2013-03-06 12:48:46 +0530

    fixes

--- js/jquery/bxsliderscript.js
+++ js/jquery/bxsliderscript.js
@@ -3,7 +3,7 @@ jQuery(document).ready(function($){
 	  var slider=jQuery('#slider').bxSlider({
 		  auto:true,
 		  //autoHover:true,
-	      pause:70000,
+	      pause:7000,
 		  touchEnabled:true,
 		  swipeThreshold:50,
 	      speed:800,
