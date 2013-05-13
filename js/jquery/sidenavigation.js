dead9a8397814904f60e5677aa3a433809709cd7
ankit4mobikasa <ankit@mobikasa.com>
2013-05-13 11:42:28 +0530

    logo

--- js/jquery/sidenavigation.js
+++ js/jquery/sidenavigation.js
@@ -234,7 +234,7 @@ function initiateshareurl(sharetype)
     });
     //console.log(cfscreen.find('img.shareit').length);
     if(_curshareimg.length == 0)
-        _curshareimg[_curshareimg.length] = 'http://staging.yogasmoga.com/yogasmoga_gold.jpg';
+        _curshareimg[_curshareimg.length] = 'http://yogasmoga.com/yogasmoga_gold.jpg';
     
     //console.log(_curshareimg[0]);
     
@@ -300,7 +300,7 @@ function shareurlmain()
 //            else
 //                _curshareimg[0] = cfscreen.find('img.shareit:first').attr('src');
 //        }
-        console.log(_curshareimg[0]);
+        //console.log(_curshareimg[0]);
         
         window.open('http://www.facebook.com/sharer.php?s=100&p[url]=' + encodeURIComponent(_currentshareurl) + '&p[images][0]=' + _curshareimg[0] + '&p[title]=YOGASMOGA | ' + _cursharedesc + '&p[summary]=' + encodeURIComponent(_cursharesummary),'Share_on_Faceook','toolbar=0,status=0,menubar=0,width=600,height=300,left=' + (_winW - 600) / 2 + ',top=' + (_winH - 300) / 2);
         break;
