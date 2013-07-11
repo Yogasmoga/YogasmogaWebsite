5a88e234c49595af881a09dd08f7ce9c5a0323e7
ankit4mobikasa <ankit@mobikasa.com>
2013-07-11 16:04:00 +0530

    pgrid color selector

--- js/jquery/mycategory.js
+++ js/jquery/mycategory.js
@@ -205,6 +205,7 @@ jQuery(document).ready(function($){
    
     $("#mycategory_products div.item").each(function(){
         cnt = $(this).find("td.colorpicker a>div").length;
+        $(this).find("td.colorpicker a>div::nth-child(5n)").css('margin-right', '0');
         if(cnt < 5)
         {
             pad = ((230 - (cnt * 46) - 6) / 2) + 9;
