d361b15349150b693e05983d2df5844220ecf919
ankit4mobikasa <ankit@mobikasa.com>
2013-08-27 17:53:32 +0530

    toggle cont.

--- js/jquery/mycheckout.js
+++ js/jquery/mycheckout.js
@@ -177,11 +177,13 @@ jQuery(document).ready(function($){
            
     jQuery("input[type='radio'][value='paypal_express']").live('click', function(){            
             jQuery("ul#payment_form_paypal_express").show();
-            jQuery("div#stripe-payment-details").hide();        
+            jQuery("div#stripe-payment-details,a#stripe-update-payment,div#change-stripe-detail").hide();            
+                    
     });
     jQuery("input[type='radio'][value='stripe']").live('click', function(){ 
-            jQuery("div#stripe-payment-details").show();    
-            jQuery("ul#payment_form_paypal_express").hide();    
+            jQuery("div#stripe-payment-details,a#stripe-update-payment").show();
+            jQuery("ul#payment_form_paypal_express").hide(); 
+            jQuery("#stripe-update-payment").addClass('use').html('Change Payment Information');   
     }); 
 });
 
