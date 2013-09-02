232449d126326622d9e00b7535f50954017dbe66
ankit4mobikasa <ankit@mobikasa.com>
2013-09-02 12:15:15 +0530

    print font and local code

---
+++ app/code/local/Mage/Adminhtml/Block/Sales/Order/Renderer/Qtybool.php
@@ -0,0 +1,20 @@
+<?php
+class Mage_Adminhtml_Block_Sales_Order_Renderer_Qtybool extends Mage_Adminhtml_Block_Widget_Grid_Column_Renderer_Abstract
+{
+public function render(Varien_Object $row)
+{
+$value =  $row->getData($this->getColumn()->getIndex());
+
+if($value)
+{
+//return '<span style="color:green;">'.$value.'</span>';
+return '<span style="color:green;">'.'Yes'.'</span>';
+
+}else
+{
+return '<span style="color:red;">'.'No'.'</span>';
+
+}
+}
+}
+?>
