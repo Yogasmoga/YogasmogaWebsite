9e62c32b366ad603ce389eb95c03997e4113c6ad
ankit4mobikasa <ankit@mobikasa.com>
2013-10-07 16:23:44 +0530

    Sales Order Fix

--- app/code/local/Mage/Adminhtml/Block/Sales/Order/Renderer/Qtybool.php
+++ app/code/local/Mage/Adminhtml/Block/Sales/Order/Renderer/Qtybool.php
@@ -3,16 +3,17 @@ class Mage_Adminhtml_Block_Sales_Order_Renderer_Qtybool extends Mage_Adminhtml_B
 {
 public function render(Varien_Object $row)
 {
-$value =  $row->getData($this->getColumn()->getIndex());
+ $value1 =  $row->getData($this->getColumn()->getIndex());
 
-if($value)
+if($value1)
 {
-//return '<span style="color:green;">'.$value.'</span>';
+//return '<span style="color:green;">'. $value1.'</span>';
 return '<span style="color:green;">'.'Yes'.'</span>';
 
-}else
+}elseif($value1=='')
 {
-//return '<span style="color:red;">'.'No'.'</span>';
+//return '<span style="color:red;">'.(int)$value1.'</span>';
+return '<span style="color:red;">'.'No'.'</span>';
 
 }
 }
