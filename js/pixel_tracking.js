fa0d02e01342112c60f8c980255a9f8882338154
ankit4mobikasa <ankit@mobikasa.com>
2013-10-28 13:00:01 +0530

    update pixel code

--- js/pixel_tracking.js
+++ js/pixel_tracking.js
@@ -1,11 +1,11 @@
-		var fb_param = {};
-		fb_param.pixel_id = "6013735872746";
-		fb_param.value = "0.00";
-		fb_param.currency = "USD";
-		(function(){
-		var fpw = document.createElement("script");
-		fpw.async = true;
-		fpw.src = "//connect.facebook.net/en_US/fp.js";
-		var ref = document.getElementsByTagName("script")[0];
-		ref.parentNode.insertBefore(fpw, ref);
-		})();
+var fb_param = {};
+fb_param.pixel_id = '6014024174346';
+fb_param.value = '0.00';
+fb_param.currency = 'USD';
+(function(){
+  var fpw = document.createElement('script');
+  fpw.async = true;
+  fpw.src = '//connect.facebook.net/en_US/fp.js';
+  var ref = document.getElementsByTagName('script')[0];
+  ref.parentNode.insertBefore(fpw, ref);
+})();
