aadbe02448986ffa94429b4cdd1bdcd93f6b3baa
Anil Singh <mobikasadeveloper1@mobikasa.com>
2013-11-13 12:53:48 +0530

    remove log anillog

--- app/code/community/Rewardpoints/Model/Observer.php
+++ app/code/community/Rewardpoints/Model/Observer.php
@@ -129,12 +129,12 @@ class Rewardpoints_Model_Observer extends Mage_Core_Model_Abstract {
         //Mage::log('got in'.$observer->getEvent()->getCustomer()->getEntityId().'   '.strtotime($customerData['created_at']).'     '.strtotime('2013-05-13 00:00:00'),null,'testlog.log');
         if(strtotime($customerData['created_at']) < strtotime('2013-05-13 00:00:00'))
             return;
-         Mage::log(date('Y-m-d H:i:s'),null,'anillog.log');       
+       //  Mage::log(date('Y-m-d H:i:s'),null,'anillog.log');       
         if (Mage::getStoreConfig('rewardpoints/registration/registration_points', Mage::app()->getStore()->getId()) > 0){
                         $from = strtotime('2013-11-13 00:00:00');
                         $to = strtotime('2013-11-27 00:00:00');
                         $valid_reg_date = strtotime($customerData['created_at']);
-						   Mage::log($valid_reg_date.$from.$to  ,null,'anillog.log');    
+						     
                         if(!($from <= $valid_reg_date && $to >= $valid_reg_date)) {
                         return;
                         }
