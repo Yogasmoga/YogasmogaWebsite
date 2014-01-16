6b0cfdd84c37c05959cd1ce708256c2a96a47262
mobikasadeveloper2 <developer2@mobikasa.com>
2014-01-16 17:39:03 +0530

    sales report with respect to sku and generate excel and show total and create excel

--- app/code/local/Mycustommodules/Mycatalog/controllers/MyproductController.php
+++ app/code/local/Mycustommodules/Mycatalog/controllers/MyproductController.php
@@ -1456,10 +1456,10 @@ ORDER BY CONCAT((SELECT VALUE FROM customer_entity_varchar WHERE entity_id=rr.re
 //                $output .= "<tr><td style='color:#fff;background-color:black;'>VALUE</td><td colspan='4'>This combination of color and size is not available and not displayed on the product view page.</td></tr>";
                 $output .= "</table>";
                 //echo 'test';
-                echo $output;
-//                $fname = mktime();
-//                file_put_contents('customreports/'.$fname.'.xls',$output);
-//                Mage::app()->getFrontController()->getResponse()->setRedirect(str_replace("/index.php","",Mage::helper('core/url')->getHomeUrl())."customreports/".$fname.".xls");
+               // echo $output;
+                $fname = mktime();
+                file_put_contents('customreports/'.$fname.'.xls',$output);
+                Mage::app()->getFrontController()->getResponse()->setRedirect(str_replace("/index.php","",Mage::helper('core/url')->getHomeUrl())."customreports/".$fname.".xls");
             }
         }
     }
