6e9dcdb2ffefc008f666db00a73a91abb80ce544
mobikasadeveloper2 <developer2@mobikasa.com>
2014-01-21 19:14:09 +0530

    small change in report sku inventory

--- app/code/local/Mycustommodules/Mycatalog/controllers/MyproductController.php
+++ app/code/local/Mycustommodules/Mycatalog/controllers/MyproductController.php
@@ -1452,7 +1452,7 @@ ORDER BY CONCAT((SELECT VALUE FROM customer_entity_varchar WHERE entity_id=rr.re
 
 
                 $output = "<table>" ;
-                $output .= "<tr><td style='height:80px;'><img src='http://yogasmoga.com/skin/frontend/yogasmoga/yogasmoga-theme/images/logo.png' /></td><td style='vertical-align:middle' colspan='5'>SKU SALES REPORT AS OF ".date("dS M,Y")."</td></tr>";
+                $output .= "<tr><td style='height:80px;'><img src='http://yogasmoga.com/skin/frontend/yogasmoga/yogasmoga-theme/images/logo.png' /></td></tr>";
                 $productCollection = Mage::getModel('catalog/product')->getCollection()->addAttributeToFilter(array(array('attribute'=>'type_id', 'eq'=>'configurable'), array('attribute'=>'status', 'eq' => Mage_Catalog_Model_Product_Status::STATUS_DISABLED)))->setPageSize(20000);
                 //$productCollection = Mage::getModel('catalog/product')->getCollection()->addAttributeToFilter(array(array('attribute'=>'type_id', 'eq'=>'configurable'), array('attribute'=>'status', 'eq' => '2')));
                 //$productCollection = Mage::getResourceModel('catalog/product_collection')->addAttributeToFilter('type_id', array('eq' => 'configurable'));
