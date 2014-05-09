b6a389ad7215c1cf389c00cbde89e29c5e8a090a
mobikasadeveloper2 <developer2@mobikasa.com>
2014-05-09 19:42:21 +0530

    working on refer a friend comment section

--- js/new_jquery/common.js
+++ js/new_jquery/common.js
@@ -1,42 +1,46 @@
 jQuery(document).ready(function($){
-		$( "#signup" ).dialog({
-					autoOpen: false,            
-		            draggable: false,
-		            resizable: false,
-		            modal: true,
-		            dialogClass : 'firstdialog',
-		            position: { my: "center center",at: "center center"},
-		            show: {
-		                effect: "fade",
-		                duration: 1000
-		            },
-		            hide: {
-		                effect: "fade",
-		                duration: 500
-		            },
-		            open: function( event, ui ) {
-		                $("input#pfirstname").blur();
-		                $(".ui-widget-overlay").css({top:80});
-		                $(window).trigger("resize");
-		                var pW = ($(document).width() - $(this).parent().width())/2;
-		                var pH = ($(window).height() - $(this).parent().height())/2;
-		                $(this).parent().css({left:pW,top:pH+10});
-		                if ($(window).width() >= "1000") {
-		                    $("html,body").css("overflow-x","hidden");
-		                    //$(window).trigger("resize");
-		                };
-		            },
-		            close: function( event, ui ) {
-		                _checkjsnav = true;
-		                if ($(window).width() >= "1000") {
-		                    $("html,body").css("overflow-x","auto");
-		                };
-		            }
-		        });
-
+    initializesignuppop();
 		$(".footer-block").on("click","#smogi-love",function(){
 			$("#signup").dialog( "open" );			
 		});
+
+    function initializesignuppop(){
+        $( "#signup" ).dialog({
+            autoOpen: false,
+            draggable: false,
+            resizable: false,
+            modal: true,
+            dialogClass : 'firstdialog',
+            position: { my: "center center",at: "center center"},
+            show: {
+                effect: "fade",
+                duration: 1000
+            },
+            hide: {
+                effect: "fade",
+                duration: 500
+            },
+            open: function( event, ui ) {
+                $("input#pfirstname").blur();
+                $(".ui-widget-overlay").css({top:80});
+                $(window).trigger("resize");
+                var pW = ($(document).width() - $(this).parent().width())/2;
+                var pH = ($(window).height() - $(this).parent().height())/2;
+                $(this).parent().css({left:pW,top:pH+10});
+                if ($(window).width() >= "1000") {
+                    $("html,body").css("overflow-x","hidden");
+                    //$(window).trigger("resize");
+                };
+            },
+            close: function( event, ui ) {
+                _checkjsnav = true;
+                if ($(window).width() >= "1000") {
+                    $("html,body").css("overflow-x","auto");
+                };
+            }
+        });
+
+    }
 });
-	
-	
+
+
