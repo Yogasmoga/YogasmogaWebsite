dddb07194066093f9200561d2a5af8bbe4ede251
sweety <developer2@mobikasa.com>
2014-05-23 18:30:05 +0530

    shopping cart page and effects

--- js/new_jquery/product.js
+++ js/new_jquery/product.js
@@ -1,38 +1,80 @@
 jQuery(document).ready(function($){
+    scrollingLink();
+    openShoppingCart();
+
     // Category links fixed on scroll function
-    var wdth = $(".cntn-scroll").width();
-    var nav = $('.scroller_anchor');
+    function scrollingLink(){
+        var wdth = $(".cntn-scroll").width();
+        var nav = $('.scroller_anchor');
 
-    if (nav.length) {
-        var contentNav = nav.offset().top;
-        $(window).scroll(function(){
-            if($(window).scrollTop() >= contentNav && $('.cntn-scroll').css('position') != 'fixed') {
-                // Change the CSS of the scroller to hilight it and fix it at the top of the screen.
-                $('.cntn-scroll').css({
-                    'position': 'fixed',
-                    'top': '3px'
-                });
-                
-                // this is container div class
-                $(".cntn-scrol-not").css({
-                    marginLeft: wdth
-                });
-            }
-            else if($(window).scrollTop() < contentNav && $('.cntn-scroll').css('position') != 'relative') {         
-                // Change the CSS and put it back to its original position.
-                $('.cntn-scroll').css({
-                    'position': '',
-                    'top': ''
-                });
+        if (nav.length) {
+            var contentNav = nav.offset().top;
+            $(window).scroll(function(){
+                if($(window).scrollTop() >= contentNav && $('.cntn-scroll').css('position') != 'fixed') {
+                    // Change the CSS of the scroller to hilight it and fix it at the top of the screen.
+                    $('.cntn-scroll').css({
+                        'position': 'fixed',
+                        'top': '3px'
+                    });
+                    
+                    // this is container div class
+                    $(".cntn-scrol-not").css({
+                        marginLeft: wdth
+                    });
+                }
+                else if($(window).scrollTop() < contentNav && $('.cntn-scroll').css('position') != 'relative') {         
+                    // Change the CSS and put it back to its original position.
+                    $('.cntn-scroll').css({
+                        'position': '',
+                        'top': ''
+                    });
 
-                // this is container div class
-                $(".cntn-scrol-not").css({
-                    marginLeft: ''
-                });
-            }
+                    // this is container div class
+                    $(".cntn-scrol-not").css({
+                        marginLeft: ''
+                    });
+                }
+            });
+        };        
+    };
+
+    // Show/Hide Shopping Cart Container
+    function openShoppingCart(){
+        var shoppingWdth = $(".shopping-cart").width();
+        var bodyHght = $(window).height();
+
+        $(".open-cart").on("click", function(){
+            $(".shopping-cart").animate({
+                height: bodyHght
+            }).show( "slide", {direction: "right"}, "fast" );
+
+            $(".page").css("position", "relative").animate({
+                left: -shoppingWdth
+            });
+
+            $(".header-container").css("position", "relative");
+
+            return false;
         });
-    }
-//    else{
-//        alert("Scrolling div in not present in the page.");
-//    }
+
+        $(".continuelink").on("click", function(){
+            $(".shopping-cart").animate({
+                height: bodyHght
+            }).hide( "slide", {direction: "right"}, "fast" );
+
+            $(".page").css("position", "relative").animate({
+                left: '0'
+            });
+
+            $(".header-container").css("position", "");
+
+            return false;
+        });
+
+        $(".addedItem li").find(".close").on("click", function(){
+            $(this).parent("li").remove();
+        });
+
+    };
+
 });
