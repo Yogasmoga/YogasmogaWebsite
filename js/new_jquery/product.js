2604f0d0733e382d1e4a4510274dd9746f81461c
sweety <developer2@mobikasa.com>
2014-05-23 11:33:52 +0530

    scrolling container issue fix

--- js/new_jquery/product.js
+++ js/new_jquery/product.js
@@ -1,57 +1,38 @@
 jQuery(document).ready(function($){
-    
-    // This is for view shopping bag div
-    //var shoppingWdth = $(".shopping-cart").width();
-    //shoppingWdth += 40;
-    //var bodyHght = $(".wrapper").height();
-    
-    //$("ul.tr-menu").find("li:nth-child(4)").on("click", function(){
-       // $(".shopping-cart").show().animate({
-       //     height: bodyHght
-     //   });
-      //  $(".page").css("position", "relative").animate({
-      //      left: -shoppingWdth
-       //   });
-   // });
-
-   // $(".shopping-cart").find(".close").on("click", function(){
-     //   $(".page").removeAttr( "style" );
-    //    $(".shopping-cart").hide("slow").removeAttr( "style" );
-   // });
-
-
-    // This function is for fixing the category menu on window scroll
+    // Category links fixed on scroll function
     var wdth = $(".cntn-scroll").width();
+    var nav = $('.scroller_anchor');
 
-    $(window).scroll(function(e) {
-        // Get the position of the location where the scroller starts.
-        // Put Element "scroller_anchor" before the scroll
-        var scroller_anchor = $(".scroller_anchor").offset().top;
-        
-        // Check if the user has scrolled and the current position is after the scroller's start location and if its not already fixed at the top
-        // Apply Class "cntn-scroll" to make element scrollable 
-        if ($(this).scrollTop() >= scroller_anchor && $('.cntn-scroll').css('position') != 'fixed') {
-            // Change the CSS of the scroller to hilight it and fix it at the top of the screen.
-            $('.cntn-scroll').css({
-                'position': 'fixed',
-                'top': '3px'
-            });
-            
-            $(".cntn-scrol-not").css({
-                marginLeft: wdth
-            });
-            
-        }
-        else if ($(this).scrollTop() < scroller_anchor && $('.cntn-scroll').css('position') != 'relative') {         
-            // Change the CSS and put it back to its original position.
-            $('.cntn-scroll').css({
-                'position': '',
-                'top': ''
-            });
-            $(".cntn-scrol-not").css({
-                marginLeft: ''
-            });
-        }
-    });
+    if (nav.length) {
+        var contentNav = nav.offset().top;
+        $(window).scroll(function(){
+            if($(window).scrollTop() >= contentNav && $('.cntn-scroll').css('position') != 'fixed') {
+                // Change the CSS of the scroller to hilight it and fix it at the top of the screen.
+                $('.cntn-scroll').css({
+                    'position': 'fixed',
+                    'top': '3px'
+                });
+                
+                // this is container div class
+                $(".cntn-scrol-not").css({
+                    marginLeft: wdth
+                });
+            }
+            else if($(window).scrollTop() < contentNav && $('.cntn-scroll').css('position') != 'relative') {         
+                // Change the CSS and put it back to its original position.
+                $('.cntn-scroll').css({
+                    'position': '',
+                    'top': ''
+                });
 
+                // this is container div class
+                $(".cntn-scrol-not").css({
+                    marginLeft: ''
+                });
+            }
+        });
+    }
+//    else{
+//        alert("Scrolling div in not present in the page.");
+//    }
 });
