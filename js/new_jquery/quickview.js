7897c6e10b48c87a8cab4af31334035279e6678f
Sweety <sweety@mobikasa.com>
2014-06-27 17:58:32 +0530

    checkout designs

--- js/new_jquery/quickview.js
+++ js/new_jquery/quickview.js
@@ -53,19 +53,21 @@ function showQuickViewPopup(productid, e)
     if(_usesecureurl)
         url = securehomeUrl + 'mycatalog/myproduct/details';
 
-    if(_quickViewObjectPage[productid]){
+  if(_quickViewObjectPage[productid]){
+       
+        //alert(_quickViewObjectPage[productid]);
         jQuery("#productdetailpopup").html(_quickViewObjectPage[productid]);
         jQuery("#productdetailpopup #colorcontainer > div:first-child > table").trigger('click');
 
-        var nextIdView = parseInt(jQuery("#" + productid).closest("li").next().children("a").attr("id"));
-        var prevIdView = parseInt(jQuery("#" + productid).closest("li").prev().children("a").attr("id"));
+        var nextIdView1 = parseInt(jQuery("#" + productid).closest("li").next().children("a").attr("id"));
+        var prevIdView1 = parseInt(jQuery("#" + productid).closest("li").prev().children("a").attr("id"));
 
-        if(nextIdView > 0){
-            jQuery(".quick-next").attr("id", nextIdView).css("display", "block");
+        if(nextIdView1 > 0){
+            jQuery(".quick-next").attr("id", nextIdView1).css("display", "block");
         }
 
-        if(prevIdView > 0){
-            jQuery(".quick-prev").attr("id", prevIdView).css("display", "block");
+        if(prevIdView1 > 0){
+            jQuery(".quick-prev").attr("id", prevIdView1).css("display", "block");
         }
 
         jQuery(".quick-next").click(function(){
@@ -77,13 +79,32 @@ function showQuickViewPopup(productid, e)
         });
 
         jQuery(document).keydown(function(e){ 
+        var nextIdView1 = parseInt(jQuery("#" + productid).closest("li").next().children("a").attr("id"));
+        var prevIdView1 = parseInt(jQuery("#" + productid).closest("li").prev().children("a").attr("id"));
             e.preventDefault();
-            if (e.which == 37) {
-                jQuery(".quick-prev").trigger('click');
+             var PrevID = jQuery('.quick-prev').attr('id');
+             if(PrevID > 0){                 
+            if (e.keyCode == 37) {
+                //jQuery(".quick-prev").trigger('click');               
+                 jQuery(".quick-prev").trigger('click')(function(){         
+                    showQuickViewPopup(PrevID); 
+                  });
+                
+                }
+                
             }
-
-            if (e.keyCode == 39) {
-                alert(jQuery(".quick-next").attr("id"));
+            
+             var NextID = jQuery('.quick-next').attr('id');
+             if(NextID > 0){                  
+            if (e.keyCode == 39) {                      
+                 //jQuery(".quick-next").trigger('click');              
+                  jQuery(".quick-next").trigger('click')(function(){                                  
+                     showQuickViewPopup(NextID);
+                  });
+                
+                 }
+                
+                
             }
         });
     }
@@ -95,6 +116,7 @@ function showQuickViewPopup(productid, e)
         success : function(data){
 
             _quickViewObjectPage[productid] = data;
+            
             jQuery("#productdetailpopup").html(data);
             jQuery("#productdetailpopup #colorcontainer > div:first-child > table").trigger('click');
 
@@ -105,6 +127,7 @@ function showQuickViewPopup(productid, e)
             var nextIdView = parseInt(jQuery("#" + productid).closest("li").next().children("a").attr("id"));
             var prevIdView = parseInt(jQuery("#" + productid).closest("li").prev().children("a").attr("id"));
 
+//alert(nextIdView);
             if(nextIdView > 0){
                 jQuery(".quick-next").attr("id", nextIdView).css("display", "block");
             }
@@ -114,20 +137,37 @@ function showQuickViewPopup(productid, e)
             }
 
             jQuery(".quick-next").click(function(){
+                                             
                 showQuickViewPopup(jQuery(this).attr('id'));
             });
             jQuery(".quick-prev").click(function(){
                 showQuickViewPopup(jQuery(this).attr('id'));
             });
-
+            
             jQuery(document).keydown(function(e){ 
                 e.preventDefault();
-                if (e.which == 37) {
-                    jQuery(".quick-prev").trigger('click');
+                 var PrevID = jQuery('.quick-prev').attr('id');
+                 if(PrevID > 0){                     
+                if (e.keyCode == 37) {                  
+                    //jQuery(".quick-prev").trigger('click');
+                    jQuery(".quick-prev").trigger('click')(function(){                  
+                    showQuickViewPopup(PrevID); 
+                     });                
+                                    
+                    }   
+                    
                 }
-
+                var NextID = jQuery('.quick-next').attr('id');             
+                if(NextID >0){                
                 if (e.keyCode == 39) {
-                    jQuery(".quick-next").trigger('click');
+                    //jQuery(".quick-next").trigger('click');
+                    jQuery(".quick-next").trigger('click')(function(){                  
+                     showQuickViewPopup(NextID);                                             
+                      // showQuickViewPopup(jQuery(this).attr('id'));
+                      });
+                    
+                                     
+                }
                 }
             });
 
@@ -135,5 +175,4 @@ function showQuickViewPopup(productid, e)
     });
 
     }
-
 }
