52ad039782477f2b367e0f0d59c1cde69edb101c
mobikasadeveloper2 <mobikasadeveloper2@mobikasa.com>
2014-06-30 12:22:37 +0530

    dashboard completed

--- js/jquery/mywatermark.js
+++ js/jquery/mywatermark.js
@@ -1,6 +1,6 @@
 var curr_input_value = '';
 jQuery(document).ready(function($){
-    $("input[type='text'][watermark],input[type='password']").blur(function () {
+    $("input[type='text'][watermark],input[type='password'],textarea[watermark]").blur(function () {
         if($(this).val() != curr_input_value)
             $(this).attr("usermodified","1");
         if($(this).val() == "")
@@ -13,7 +13,7 @@ jQuery(document).ready(function($){
                 $(this).val($(this).attr("watermark")).addClass('watermark');   
         }
     });
-    $("input[type='text'][watermark],input[type='password']").focus(function () {       
+    $("input[type='text'][watermark],input[type='password'],textarea[watermark]").focus(function () {       
         if(!$(this).attr("usermodified"))
         {
             if ($(this).val() == $(this).attr("watermark"))
@@ -25,7 +25,7 @@ jQuery(document).ready(function($){
 });
 
 function applywatermark() {    
-    jQuery.each(jQuery("input[type='text'][watermark]"), function () {
+    jQuery.each(jQuery("input[type='text'][watermark],textarea[watermark]"), function () {
         if(!jQuery(this).attr("usermodified"))
         {
             if (jQuery(this).val() == '' || jQuery(this).val() == jQuery(this).attr("watermark"))
@@ -37,7 +37,7 @@ function applywatermark() {
 }
 
 function removewatermarks() {
-    jQuery.each(jQuery("input[type='text'][watermark]"), function () {
+    jQuery.each(jQuery("input[type='text'][watermark],textarea[watermark]"), function () {
         if(!jQuery(this).attr("usermodified"))
         {
             if (jQuery(this).val() == '' || jQuery(this).val() == jQuery(this).attr("watermark"))
