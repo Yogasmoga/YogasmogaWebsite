0f6eea78ca99a5e9fe7770ae419abc2c7f9173b6
Neeraj Dang <neeraj@mobikasa.com>
2014-07-10 11:32:28 +0530

    updates

--- app/code/local/Mycustommodules/Mynewtheme/controllers/EmailUsController.php
+++ app/code/local/Mycustommodules/Mynewtheme/controllers/EmailUsController.php
@@ -6,31 +6,30 @@ class Mycustommodules_Mynewtheme_EmailUsController extends Mage_Core_Controller_
     {
         $data = array();
         print_r($this->getRequest()->getPost());
-        $name= $this->getRequest()->getPost('name');
-        //$filedata= $this->getRequest()->getPost('filename');
-        if(isset($name))
-        {	
-                $error = false;
-                $files = array();
-                $baseDir = Mage::getBaseDir();
-                $uploaddir = $baseDir.DS.'uploads'.DS;
-                foreach($_FILES as $file)
-                {
-                        if(move_uploaded_file($file['tmp_name'], $uploaddir .basename($file['name'])))
-                        {
-                                $files[] = $uploaddir .$file['name'];
-                        }
-                        else
-                        {
-                            $error = true;
-                        }
-                }
-                $data = ($error) ? array('error' => 'There was an error uploading your files') : array('files' => $files);
-        }
-        else
-        {
-                $data = array('success' => 'Form was submitted', 'formData' => $_POST);
-        }
+//        $name= $this->getRequest()->getPost('name');
+//        if(isset($name))
+//        {	
+//                $error = false;
+//                $files = array();
+//                $baseDir = Mage::getBaseDir();
+//                $uploaddir = $baseDir.DS.'uploads'.DS;
+//                foreach($_FILES as $file)
+//                {
+//                        if(move_uploaded_file($file['tmp_name'], $uploaddir .basename($file['name'])))
+//                        {
+//                                $files[] = $uploaddir .$file['name'];
+//                        }
+//                        else
+//                        {
+//                            $error = true;
+//                        }
+//                }
+//                $data = ($error) ? array('error' => 'There was an error uploading your files') : array('files' => $files);
+//        }
+//        else
+//        {
+//                $data = array('success' => 'Form was submitted', 'formData' => $_POST);
+//        }
 
         echo json_encode($data);
 
