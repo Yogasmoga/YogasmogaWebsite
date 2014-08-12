ccd9b54e365850d63b70c1f544748aa7cd3be7a5
Sweety <sweety@mobikasa.com>
2014-08-12 13:52:53 +0530

    menu, grid leftnav, home and landing images

--- js/new_jquery/emailus.js
+++ js/new_jquery/emailus.js
@@ -52,7 +52,6 @@ jQuery(document).ready(function($){
                 data.append('email', email);
                 jQuery('#loader').show();
                 jQuery('#submitfile').hide();
-                jQuery("#submitfile").after("<img class='f-right' style='margin: 10px 20px 0px 0px;' id='procImg' src='" + skinUrl + "images/new-loader.gif' />");
                 jQuery.ajax({
                     url: 'mynewtheme/emailus/sendmail',
                     type: 'POST',
@@ -78,7 +77,6 @@ jQuery(document).ready(function($){
                         }
                         jQuery('#loader').hide();
                         jQuery('#submitfile').show();
-                        jQuery("#procImg").remove();
                     },
                     error: function(jqXHR, textStatus, errorThrown)
                     {
