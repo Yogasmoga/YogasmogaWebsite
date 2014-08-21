972cc388fcbcbf84166699a84df930e6a46a9112
mobikasadeveloper2 <developer2@mobikasa.com>
2014-08-21 16:47:20 +0530

    onlive site nosmogicategory have 3 categories

--- js/mobile/jquerycustom.js
+++ js/mobile/jquerycustom.js
@@ -8,4 +8,8 @@ jQuery(document).ready(function($){
         });
     }, 500);
 
+    jQuery("h2.IN").next().find(".price").css("color","rgba(255, 0, 0, 0.99)");
+    jQuery(".product-name.IN").next().next().find("span.price").css("color","rgba(255, 0, 0, 0.99)");
+
+
 });
