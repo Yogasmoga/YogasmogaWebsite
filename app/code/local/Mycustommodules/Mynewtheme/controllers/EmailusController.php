88431a0d971703bab8334d9309b206ebea1869e3
mobikasadeveloper2 <developer2@mobikasa.com>
2014-09-25 12:03:56 +0530

    email us

--- app/code/local/Mycustommodules/Mynewtheme/controllers/EmailusController.php
+++ app/code/local/Mycustommodules/Mynewtheme/controllers/EmailusController.php
@@ -77,12 +77,13 @@ class Mycustommodules_Mynewtheme_EmailusController extends Mage_Core_Controller_
 //        $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
 //        mail($toemail,$subject,$html,$headers);
         //echo $html;
-       $this->sendemail($html);
+        $customeremail = $email;
+       $this->sendemail($html,$customeremail,$name);
        echo json_encode($data);
 
     }
     
-    public function sendemail($html)
+    public function sendemail($html,$customeremail,$name)
     {
         $translate = Mage::getSingleton('core/translate');
         $translate->setTranslateInline(false);
@@ -97,8 +98,8 @@ class Mycustommodules_Mynewtheme_EmailusController extends Mage_Core_Controller_
             'name'  => 'YOGASMOGA'
         );
         $sender  = array(
-            'name' => 'YOGASMOGA',
-            'email' => 'manish@yogasmoga.com'
+            'name' => $name,
+            'email' => $customeremail
         );
         //echo "<pre>";print_r($email); die('test');
         $email->setDesignConfig(array('area'=>'frontend', 'store'=> Mage::app()->getStore()->getId()))
