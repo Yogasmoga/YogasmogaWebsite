b6b91c470b7155b5c7bc13114ab027b13f55b74d
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2015-04-14 17:07:03 +0530

    gender added to popup, rangoli arrow goes to home page

--- js/new_jquery/signing.js
+++ js/new_jquery/signing.js
@@ -340,6 +340,8 @@ function  createCustomerAccount()
     var lname = jQuery.trim(jQuery("#lname").val());
     var email_id = jQuery.trim(jQuery("#signup_email").val());
     var pwd = jQuery.trim(jQuery("#s_password").val());
+    var gender = jQuery.trim(jQuery("input[name='gender']:checked").val());
+
     var cpassword = pwd;
     var is_subscribed = jQuery("#in_touch").val();
     if(window.location.href.indexOf('https://') >= 0)
@@ -358,7 +360,7 @@ function  createCustomerAccount()
     jQuery.ajax({
         url     :   url,
         type    :   'POST',
-        data    :   {'firstname':fname,'lastname':lname,'email':email_id,'password':pwd,'confirmation':cpassword,'is_subscribed':is_subscribed},
+        data    :   {'firstname':fname,'lastname':lname,'email':email_id,'password':pwd,'confirmation':cpassword,'is_subscribed':is_subscribed,'gender':gender},
         beforeSend: function() {
             jQuery("#sign-up-form .form-loader").html("<img src='/skin/frontend/new-yogasmoga/yogasmoga-theme/images/new-loader.gif' style='width:16px;' />");
             jQuery("#sign-up-button").parent().hide();
