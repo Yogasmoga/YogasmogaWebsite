74de8bdb3e7199a2a1fa08c0763f59696c51fc63
ashutosh <ashutosh.options4u@gmail.com>
2015-06-04 23:31:32 +0530

    productfeed update

--- productfeed/index.php
+++ productfeed/index.php
@@ -1,5 +1,7 @@
 <?php
     ini_set('max_execution_time', 300); //300 seconds = 5 minutes
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
 
     require '../app/Mage.php';
     Mage::app();
