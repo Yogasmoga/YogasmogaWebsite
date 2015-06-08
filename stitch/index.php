a06b3c2ea011fb9edb00305f468900d53171277a
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2015-06-08 19:53:44 +0530

    stitch reportupdate

--- stitch/index.php
+++ stitch/index.php
@@ -229,7 +229,7 @@ ksort($all_products);
                 $unit_price = $color_data[0]['unit_price'];
                 echo "<td style='text-align: left'>$ " . round($unit_price,2) . "</td>";
                 $net_total = $total * $unit_price;
-                echo "<td style='text-align: left'>$ > " . round($net_total,2) . "</td>";
+                echo "<td style='text-align: left'>$ " . number_format($net_total, 2, '.', ''). "</td>";
 
                 $total_inventories += $total * $unit_price;
             }
