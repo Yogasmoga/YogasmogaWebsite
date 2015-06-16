c85b0c87844bb1db07455152ed5b4b7f8e988450
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2015-06-16 19:25:36 +0530

    nominate teacher grid update

--- app/code/local/Ysindia/Nominateinstructor/Block/Adminhtml/Nominateinstructor/Grid.php
+++ app/code/local/Ysindia/Nominateinstructor/Block/Adminhtml/Nominateinstructor/Grid.php
@@ -27,55 +27,74 @@ class Ysindia_Nominateinstructor_Block_Adminhtml_Nominateinstructor_Grid extends
           'index'     => 'nominateinstructor_id',
       ));
 
-      $this->addColumn('your_name', array(
-          'header'    => Mage::helper('nominateinstructor')->__('Name'),
+      $this->addColumn('your_first_name', array(
+          'header'    => Mage::helper('nominateinstructor')->__('Y. First Name'),
           'align'     =>'left',
-          'index'     => 'your_name',
-		  'renderer'	=> new Ysindia_Nominateinstructor_Block_Adminhtml_Renderer_CustomerName()
+          'index'     => 'your_first_name'
+//		  'renderer'	=> new Ysindia_Nominateinstructor_Block_Adminhtml_Renderer_CustomerName()
       ));
+
+      $this->addColumn('your_last_name', array(
+          'header'    => Mage::helper('nominateinstructor')->__('Y. Last Name'),
+          'align'     =>'left',
+          'index'     => 'your_last_name',
+      ));
+
 	  $this->addColumn('your_email', array(
-          'header'    => Mage::helper('nominateinstructor')->__('Email'),
+          'header'    => Mage::helper('nominateinstructor')->__('Y. Email'),
           'align'     =>'left',
           'index'     => 'your_email',
       ));
-	  
-	  $this->addColumn('name', array(
-          'header'    => Mage::helper('nominateinstructor')->__('Instructor Name'),
-          'align'     =>'left',
-          'index'     => 'name',
-		  'renderer'	=> new Ysindia_Nominateinstructor_Block_Adminhtml_Renderer_InstructorName()
-		  
+
+      $this->addColumn('your_gender', array(
+          'header'    => Mage::helper('nominateinstructor')->__('Y. Gender'),
+          'width'     => '50px',
+          'index'     => 'your_gender',
       ));
-	  
+
+	  $this->addColumn('instructor_first_name', array(
+          'header'    => Mage::helper('nominateinstructor')->__('I. First Name'),
+          'align'     =>'left',
+          'index'     => 'instructor_first_name'
+//		  'renderer'	=> new Ysindia_Nominateinstructor_Block_Adminhtml_Renderer_InstructorName()
+      ));
+
+      $this->addColumn('instructor_last_name', array(
+          'header'    => Mage::helper('nominateinstructor')->__('I. Last Name'),
+          'align'     =>'left',
+          'index'     => 'instructor_last_name',
+      ));
+
 		$this->addColumn('instructor_email', array(
-          'header'    => Mage::helper('nominateinstructor')->__('Instructor Email'),
+          'header'    => Mage::helper('nominateinstructor')->__('I. Email'),
           'align'     =>'left',
           'index'     => 'instructor_email',
       ));
-	  $this->addColumn('created_time', array(
+
+      $this->addColumn('content', array(
+			'header'    => Mage::helper('nominateinstructor')->__('Description'),
+			'width'     => '250px',
+			'index'     => 'content',
+      ));
+
+      $this->addColumn('created_time', array(
           'header'    => Mage::helper('nominateinstructor')->__('Created Time'),
           'align'     =>'left',
           'index'     => 'created_time',
       ));
-	  /*
-      $this->addColumn('content', array(
-			'header'    => Mage::helper('nominateinstructor')->__('Item Content'),
-			'width'     => '150px',
-			'index'     => 'content',
-      ));
-	  */
 
-      $this->addColumn('status', array(
-          'header'    => Mage::helper('nominateinstructor')->__('Status'),
-          'align'     => 'left',
-          'width'     => '80px',
-          'index'     => 'status',
-          'type'      => 'options',
-          'options'   => array(
-              1 => 'Enabled',
-              2 => 'Disabled',
-          ),
-      ));
+//
+//      $this->addColumn('status', array(
+//          'header'    => Mage::helper('nominateinstructor')->__('Status'),
+//          'align'     => 'left',
+//          'width'     => '80px',
+//          'index'     => 'status',
+//          'type'      => 'options',
+//          'options'   => array(
+//              1 => 'Enabled',
+//              2 => 'Disabled',
+//          ),
+//      ));
 	  
         $this->addColumn('action',
             array(
