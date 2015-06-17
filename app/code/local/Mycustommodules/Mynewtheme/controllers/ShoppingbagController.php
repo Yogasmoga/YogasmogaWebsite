90e563163b5534f118b2fe5a566b5f4eb56d6e81
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2015-06-17 19:53:54 +0530

    vivacity popup update

--- app/code/local/Mycustommodules/Mynewtheme/controllers/ShoppingbagController.php
+++ app/code/local/Mycustommodules/Mynewtheme/controllers/ShoppingbagController.php
@@ -1750,9 +1750,17 @@ class Mycustommodules_Mynewtheme_ShoppingbagController extends Mage_Core_Control
 
 
 /********************* now showing all cart items *********************/
+        $vivacityConfigurableIds = array(2566,1990,1846,1843);
+        $vivacityFound = false;
 
         foreach($minidetails['items'] as $item)
         {
+            foreach($vivacityConfigurableIds as $vivacityId){
+                if($item['pid']==$vivacityId){
+                    $vivacityFound = true;
+                    break;
+                }
+            }
 
             $html .='<li id="'.$item['itemid'].'" availableqty="'.$item['pavailableqty'].'" backorder="'.$item['preorder'].'" instock="'.$item['instock'].'">
                 <a href="'.$item['producturl'].'"><span class="wdth100"><img alt="'.$item['name'].'" src="'.substr($item['imageurl'], 1).'" ></span></a>
@@ -2153,17 +2161,15 @@ class Mycustommodules_Mynewtheme_ShoppingbagController extends Mage_Core_Control
         }
 
         // end to show braclet
-        $html .= '</ul>
-                                    </div>
-                                    <!-- productadded -->
-                                    ';
+        if($vivacityFound)
+            $vivacityStr = '<span style="display:none;" class="vivacity" rel="yes"></span>';
+        else
+            $vivacityStr = '<span  style="display:none;" class="vivacity" rel="no"></span>';
 
+        // end to show braclet
+        $html .= '</ul>' . $vivacityStr . '</div>';
 
         return $html;
-        //return $jshtml.$html;
-        //print_r($html);die('test');
-        //echo $html;
-        //echo json_encode(array("html" => $html));
     }
 
     protected function fastcreateshoppingbaghtml()
