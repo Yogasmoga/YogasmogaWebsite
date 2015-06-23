9f62b3a62b73c12867345c5a665616481b92815d
YSINFO <info@yogasmoga.com>
2015-06-23 14:20:18 +0530

    update

--- rangoli/wp-login.php
+++ rangoli/wp-login.php
@@ -12,7 +12,7 @@
 require( dirname(__FILE__) . '/wp-load.php' );
 
 // Redirect to https login if forced to use SSL
-if ( force_ssl_admin() && ! is_ssl() ) {
+/*if ( force_ssl_admin() && ! is_ssl() ) {
 	if ( 0 === strpos($_SERVER['REQUEST_URI'], 'http') ) {
 		wp_redirect( set_url_scheme( $_SERVER['REQUEST_URI'], 'https' ) );
 		exit();
@@ -20,7 +20,7 @@ if ( force_ssl_admin() && ! is_ssl() ) {
 		wp_redirect( 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'] );
 		exit();
 	}
-}
+}*/
 
 /**
  * Output the login page header.
