9608a01212fd88c9680006316f7cbabee9df47e7
YSINFO <info@yogasmoga.com>
2015-06-25 18:59:26 +0530

    cart controller and menu.js update

--- js/mobile/menu.js
+++ js/mobile/menu.js
@@ -124,126 +124,128 @@ $j(document).ready(function () {
 
 /*////////////////////////////////////////////////////////////////////////////////////////////*/
 
-        jQuery(document).ready(function($){
-           $(".already_signed_up").click(function(){
-               openLogin();
-               closeSignup();
-           });
+jQuery(document).ready(function($){
+    $(".already_signed_up").click(function(){
+        openLogin();
+        closeSignup();
+    });
 
 
-            $("#sizechart").click(function(){
-                var size_chart = $(".lg-size-chart").html();
-                $(".size_chart_popup").fadeIn();
-                $(".size_chart_popup .chart_container").html(size_chart);
-            });
-            $(".close_size_cart").click(function(){
-                $(".size_chart_popup").fadeOut();
-            });
-            $(".video-block.f-right > img").click(function(){
-                $(".html-vid-pop.html-fit-vid-popup").fadeIn();
-            });
+    $("#sizechart").click(function(){
+        var size_chart = $(".lg-size-chart").html();
+        $(".size_chart_popup").fadeIn();
+        $(".size_chart_popup .chart_container").html(size_chart);
+    });
+    $(".close_size_cart").click(function(){
+        $(".size_chart_popup").fadeOut();
+    });
+    $(".video-block.f-right > img").click(function(){
+        $(".html-vid-pop.html-fit-vid-popup").fadeIn();
+    });
 
-            $(".close_video_popup").click(function(){
-                $(".html-vid-pop").fadeOut();
-            });
-            $(".vid-pl-btn").click(function(){
-                $(".html-des-vid-popup").fadeIn();
-            });
+    $(".close_video_popup").click(function(){
+        $(".html-vid-pop").fadeOut();
+    });
+    $(".vid-pl-btn").click(function(){
+        $(".html-des-vid-popup").fadeIn();
+    });
 
-            $(".arrow-icon").click(function(){
-                $(this).toggleClass("active");
-                $(".account-nav.l-align").slideToggle();
-            });
-            $(".story-box .txt-cnt a").click(function(e){
-                e.preventDefault();
-            });
-            $(".story-box .textContainer a").click(function(e){
-                e.preventDefault();
-            });
+    $(".arrow-icon").click(function(){
+        $(this).toggleClass("active");
+        $(".account-nav.l-align").slideToggle();
+    });
+    $(".story-box .txt-cnt a").click(function(e){
+        e.preventDefault();
+    });
+    $(".story-box .textContainer a").click(function(e){
+        e.preventDefault();
+    });
 
-            $(".story-box .txt-cnt").click(function(){
-                $(this).next().show();
-                $(".story-box .txt-cnt").next().removeClass("has_slider");
-                $(this).next().addClass("has_slider");
-                $(".story-box .inside-content").not($(this).next()).hide();
-                var top = $(this).offset().top;
-                $(window).scrollTop(top-88);
-                $(".story-box").find(".close-icn").hide();
-                $(this).parent().find(".close-icn").fadeIn();
-                $(".flexslider").resize();
-                $(".flexslider .slides").css("margin-left",0);
-            });
-            $(".story-box .close-icn").click(function(){
-                $(this).fadeOut();
-                $(this).next().next().hide();
-            });
-            $("p.apply_promo_label > span").click(function(){
-                $(".apply_promo_code_div").show();
-                $(this).parent().remove();
-            });
-             
-			  $("p.apply_smogi_label > span").click(function(){
-                $(".apply_smogi_code_div").show();
-                $(this).parent().remove();
-            });
+    $(".story-box .txt-cnt").click(function(){
+        $(this).next().show();
+        $(".story-box .txt-cnt").next().removeClass("has_slider");
+        $(this).next().addClass("has_slider");
+        $(".story-box .inside-content").not($(this).next()).hide();
+        var top = $(this).offset().top;
+        $(window).scrollTop(top-88);
+        $(".story-box").find(".close-icn").hide();
+        $(this).parent().find(".close-icn").fadeIn();
+        $(".flexslider").resize();
+        $(".flexslider .slides").css("margin-left",0);
+    });
+    $(".story-box .close-icn").click(function(){
+        $(this).fadeOut();
+        $(this).next().next().hide();
+    });
+    $("p.apply_promo_label > span").click(function(){
+        $("#error_msg_coupon").html("");
+        $(".apply_promo_code_div").show();
+        $(this).parent().remove();
+    });
 
-            $(".arrow-tag.narrow").click(function(){
-                $(".arrow-tag.narrow").next().not($(this).next()).slideUp();
-                $(".arrow-tag.narrow").not($(this)).removeClass("is-active");
-                $(this).next().slideToggle();
-            });
-            $(".buck-balance-title .arrow-tag").click(function(){
-                $(".buck-balance-title .arrow-tag").removeClass("is-active");
-                $(".buck-balance-title").not($(this).parent()).next().hide();
-                $(".close-icn").hide();
-                $(".smogi-icn").removeClass("active");
-                $(".buck-balance-title .close-icn").not($(this).parent().find(".close-icn")).fadeOut();
-                $(this).parent().next().slideDown(0);
-                $(this).parent().find(".close-icn").fadeIn();
-                $(".read-more").show();
-                $(".smogi-content .hidden-txt").hide();
-                $(this).parent().find(".smogi-icn").addClass("active");
-                var top = $(this).parent().offset().top;
-                $(window).scrollTop(top-88);
-            });
-            $(".buck-balance-title .close-icn").click(function(){
-                $(".smogi-icn").removeClass("active");
-                $(".buck-balance-title .arrow-tag").removeClass("is-active");
-                $(this).fadeOut();
-                $(this).parent().find(".smogi-icn").removeClass("active");
-                $(this).closest(".buck-balance-title").next().slideUp();
-            });
-            $(".ys_static_page_dropdown li a").click(function(e){
-                e.preventDefault();
-                $(".arrow-icon").click();
-                $(".story-box .close-icn").click();
-                var heading = $(this).attr("data-heading");
-                var obj_id = $(this).attr("rel");
-                var obj =$("#" + obj_id);
-                if(obj.length>0) {
-                    var top = obj.offset().top;
-                    top = top - 88;
-                    $("html,body").animate({
-                        scrollTop: top
-                    }, 200);
-                    $("#" + obj_id + " .txt-cnt").click();
-                    $(".sign-in-box h1").html(heading);
-                }
-                else{
-                    window.location = $(this).attr("href");
-                }
-            });
+    $("p.apply_smogi_label > span").click(function(){
+        $("#error_msg_smogi").html("");
+        $(".apply_smogi_code_div").show();
+        $(this).parent().remove();
+    });
 
-            $(".see_our_colors").click(function(){
-                $(".colors_list").slideToggle();
-                $(this).find("span").toggleClass("active");
-            });
-            $(".customer_gender_selection > div > label").click(function(){
-                $(".customer_gender_selection > div > label").removeClass("selected");
-                $(this).addClass("selected");
-            });
+    $(".arrow-tag.narrow").click(function(){
+        $(".arrow-tag.narrow").next().not($(this).next()).slideUp();
+        $(".arrow-tag.narrow").not($(this)).removeClass("is-active");
+        $(this).next().slideToggle();
+    });
+    $(".buck-balance-title .arrow-tag").click(function(){
+        $(".buck-balance-title .arrow-tag").removeClass("is-active");
+        $(".buck-balance-title").not($(this).parent()).next().hide();
+        $(".close-icn").hide();
+        $(".smogi-icn").removeClass("active");
+        $(".buck-balance-title .close-icn").not($(this).parent().find(".close-icn")).fadeOut();
+        $(this).parent().next().slideDown(0);
+        $(this).parent().find(".close-icn").fadeIn();
+        $(".read-more").show();
+        $(".smogi-content .hidden-txt").hide();
+        $(this).parent().find(".smogi-icn").addClass("active");
+        var top = $(this).parent().offset().top;
+        $(window).scrollTop(top-88);
+    });
+    $(".buck-balance-title .close-icn").click(function(){
+        $(".smogi-icn").removeClass("active");
+        $(".buck-balance-title .arrow-tag").removeClass("is-active");
+        $(this).fadeOut();
+        $(this).parent().find(".smogi-icn").removeClass("active");
+        $(this).closest(".buck-balance-title").next().slideUp();
+    });
+    $(".ys_static_page_dropdown li a").click(function(e){
+        e.preventDefault();
+        $(".arrow-icon").click();
+        $(".story-box .close-icn").click();
+        var heading = $(this).attr("data-heading");
+        var obj_id = $(this).attr("rel");
+        var obj =$("#" + obj_id);
+        if(obj.length>0) {
+            var top = obj.offset().top;
+            top = top - 88;
+            $("html,body").animate({
+                scrollTop: top
+            }, 200);
+            $("#" + obj_id + " .txt-cnt").click();
+            $(".sign-in-box h1").html(heading);
+        }
+        else{
+            window.location = $(this).attr("href");
+        }
+    });
 
-        });
+    $(".see_our_colors").click(function(){
+        $(".colors_list").slideToggle();
+        $(this).find("span").toggleClass("active");
+    });
+    $(".customer_gender_selection > div > label").click(function(){
+        $(".customer_gender_selection > div > label").removeClass("selected");
+        $(this).addClass("selected");
+    });
+
+});
 
 
 jQuery(window).load(function(){
