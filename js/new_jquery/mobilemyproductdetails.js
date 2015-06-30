27e3afa056a5a04d0076dddf96b75cf4ec986105
YSINFO <info@yogasmoga.com>
2015-06-30 16:37:17 +0530

    insert bra option touchstart

--- js/new_jquery/mobilemyproductdetails.js
+++ js/new_jquery/mobilemyproductdetails.js
@@ -866,7 +866,7 @@ function selectfirstsizeonload() {
 function insertBraOption() {
     _braSelected = 0;
     jQuery("body").find("#includeoption div:nth-child(2)").trigger("click");
-    jQuery("body").on("click", "#includeoption div", function () {
+    jQuery("body").on("touchstart click", "#includeoption div", function () {
         var braValue = parseInt(jQuery("#includeoption div:nth-child(1)").attr("value"));
         jQuery(this).addClass("selected").siblings().removeClass("selected");
         var textvalue = jQuery.trim(jQuery(this).text());
