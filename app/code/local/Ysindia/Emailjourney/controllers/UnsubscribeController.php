edf158a4e2391a3ca03426dc409d3ac29a694bb5
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2015-07-08 15:07:03 +0530

    email journey unsubscribe controller added

---
+++ app/code/local/Ysindia/Emailjourney/controllers/UnsubscribeController.php
@@ -0,0 +1,62 @@
+<?php
+
+class Ysindia_Emailjourney_UnsubscribeController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+        $customerId = $this->getRequest()->getParam('id');
+
+        if (isset($customerId) && strlen($customerId)>0) {
+
+            $customer = Mage::getModel('customer/customer')->load($customerId);
+
+            if (isset($customer)) {
+
+                $resource = Mage::getSingleton('core/resource');
+
+                $readConnection = $resource->getConnection('core_read');
+                $query = "SELECT id FROM unsubscribed_customers where customer_id=$customerId";
+
+                $row = $readConnection->fetchAll($query);
+
+                if ($row) {
+?>
+                    <div style="font:30px/36px ITCAvantGardeProBk;color:#555555;width:100%;height:381px;text-align:center;  padding: 90px 0;  box-sizing: border-box;"
+                         class="unsubscribe-page">
+                        You are already unsubscribed.<br/>However, you can always contact us<br/>at
+                        <a href="mailto:hello@yogasmoga.com" style="color:#666;text-decoration:none">hello@yogasmoga.com</a> to be included<br/>back into our mailing list. <br/><br/>
+
+                        Have a breezy day.
+
+
+                    </div>
+
+                <?php
+                } else {
+                    $writeConnection = $resource->getConnection('core_write');
+
+                    $created_at = date('Y-m-d h:i:s');
+
+                    $query = "insert into unsubscribed_customers(customer_id, email, created_at) values($customerId, '" . $customer->getEmail() . "','$created_at')";
+
+                    $result = $writeConnection->query($query);
+?>
+                    <div style="font:30px/36px ITCAvantGardeProBk;color:#555555;width:100%;height:381px;text-align:center;  padding: 90px 0;  box-sizing: border-box;"
+                         class="unsubscribe-page">
+                        We're sad to see one of our SMOGIs leave us.<br/>However, you can always contact us<br/>at
+                        <a href="mailto:hello@yogasmoga.com" style="color:#666;text-decoration:none">hello@yogasmoga.com</a> to be included<br/>back into our mailing list. <br/><br/>
+
+                        Have a breezy day.
+
+
+                    </div>
+<?php
+                }
+            }
+        }
+        else
+            return $this->_redirect('/');
+    }
+}
+
+?>
