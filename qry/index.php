3dc90459f69185674b5275be84ac259c752bb00a
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2015-07-13 19:05:51 +0530

    customersbyinactivity added

---
+++ qry/index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>Manage Reports</head>
+    <body>
+
+        <ul>
+            <li><a href="customersbyorder.php">Get customers between dates according to order amount</a></li>
+            <li><a href="customersbyproducts.php.php">Get customers who purchased particular products</a></li>
+            <li><a href="customersbyinactivity.php">Get customers who haven't purchased since date</a></li>
+        </ul>
+
+    </body>
+</html>
