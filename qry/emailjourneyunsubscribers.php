9d680370edcd6cdf3395865aa58dc2139d96a748
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2015-07-28 13:40:26 +0530

    email journey unsubscribed page added

---
+++ qry/emailjourneyunsubscribers.php
@@ -0,0 +1,65 @@
+<?php
+
+if(isset($_REQUEST['from_date'])) {
+
+    require_once '../app/Mage.php';
+    Mage::app();
+    umask(0);
+
+    $date = $_REQUEST['date'];
+
+    $resource = Mage::getSingleton('core/resource');
+    $readConnection = $resource->getConnection('core_read');
+
+    $query = 'select * from unsubscribed_customers';
+
+    $rows = $readConnection->fetchAll($query);
+
+    header('Content-Type: text/csv; charset=utf-8');
+    header('Content-Disposition: attachment; filename=unsubscribed.csv');
+
+    $fp = fopen('php://output', 'w');
+
+    fputcsv($fp, array("Customers unsubscribed on " . $date));
+    fputcsv($fp, array(''));
+
+    fputcsv($fp, array(''));
+    fputcsv($fp, array(''));
+
+    foreach ($rows as $row) {
+        $email = $row['email'];
+        $customerId = $row['customer_id'];
+        fputcsv($fp, array($customerId, $email, $date));
+    }
+
+    fclose($fp);
+
+    //readfile('customers.txt');
+    exit;
+}
+else{
+?>
+
+<html>
+    <body>
+
+        <br/><a href="index.php">Home</a><br/><br/>
+
+        <form method="post" action="emailjourneyunsubscribers.php">
+
+            <h3>Download customers</h3>
+
+            <table style="width:500px;" border="0">
+                <tr>
+                    <td style="width:150px;">Date</td>
+                    <td><input type="text" name="from_date"/> (mm/dd/yyyy)</td>
+                </tr>
+                <tr>
+                    <td colspan="2"><input type="submit" value="Get Records"/> </td>
+                </tr>
+            </table>
+        </form>
+    </body>
+</html>
+
+<?php } ?>
--- qry/index.php
+++ qry/index.php
@@ -6,6 +6,7 @@
             <li><a href="customersbyorder.php">Get customers between dates according to order amount</a></li>
             <li><a href="customersbyproducts.php.php">Get customers who purchased particular products</a></li>
             <li><a href="customersbyinactivity.php">Get customers who haven't purchased since date</a></li>
+            <li><a href="emailjourneyunsubscribers.php">Customers who unsubscribed from email journey</a></li>
         </ul>
 
     </body>
