0044971df0718fc1f99451ba11cc180a6a28dfde
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2015-08-03 12:33:04 +0530

    product feed update

--- qry/productfeed/download.php
+++ qry/productfeed/download.php
@@ -1,6 +1,6 @@
 <?php
 $root = "admin.yogasmoga.com";
-$file_url = "http://$root/productfeed/result.txt";
+$file_url = "http://$root/qry/productfeed/result.txt";
 header('Content-Type: application/octet-stream');
 header("Content-Transfer-Encoding: Binary");
 header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\"");
