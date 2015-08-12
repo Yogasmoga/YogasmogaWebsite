1e7baf5f77960ef1804cbfc0460d33a4e0ee5f10
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2015-08-12 15:50:00 +0530

    stitch update, inactive products added, emain journey update

--- qry/index.php
+++ qry/index.php
@@ -6,6 +6,8 @@
             <li><a href="customersbyorder.php">Get customers between dates according to order amount</a></li>
             <li><a href="customersbyproducts.php.php">Get customers who purchased particular products</a></li>
             <li><a href="customersbyinactivity.php">Get customers who haven't purchased since date</a></li>
+            <li><a href="emailjourneyunsubscribers.php">Customers who unsubscribed from email journey</a></li>
+            <li><a href="inactiveproducts.php">Inactive products</a></li>
         </ul>
 
     </body>
