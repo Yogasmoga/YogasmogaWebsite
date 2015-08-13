714e37e96bfe0745947275eea115a9c9d12e6459
YSINFO <info@yogasmoga.com>
2015-08-13 19:37:59 +0530

    static pages slider arrow

--- js/mobile/menu.js
+++ js/mobile/menu.js
@@ -285,7 +285,8 @@ jQuery(window).load(function(){
     var height = jQuery(".productimagecontainer .flexslider li img").height();
     var newSliderImg = jQuery(".new-slider li img").height();
     jQuery(".has_slider .new-slider .flex-control-nav").show();
-    jQuery(".has_slider .new-slider .flex-control-nav").css("top",newSliderImg);
+    jQuery(".has_slider .new-slider .flex-direction-nav").show();
+    jQuery(".has_slider .new-slider .flex-direction-nav").css("top",newSliderImg);
     jQuery(".productimagecontainer .flexslider").css("height",height);
     jQuery(".productimagecontainer ").css("height",height);
     var ww = jQuery(window).width();
