2be230dc34f4b750c2caf17abf36886e961951da
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2015-08-27 21:15:16 +0530

    cart/item/default reverted back

--- app/code/local/Mage/Core/Model/Store.php
+++ app/code/local/Mage/Core/Model/Store.php
@@ -953,8 +953,6 @@ class Mage_Core_Model_Store extends Mage_Core_Model_Abstract
      */
     public function formatPrice($price, $includeContainer = true)
     {
-        $includeContainer = false;
-
         if ($this->getCurrentCurrency()) {
 		    return $this->getCurrentCurrency()->format($price, array(), $includeContainer);
         }
