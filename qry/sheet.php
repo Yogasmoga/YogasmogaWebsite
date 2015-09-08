8ea0d23d4f7a658201d70aa00b79c7f2fdf566d1
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2015-09-08 16:02:15 +0530

    qry->sheet.php and div added navigagion->left

--- qry/sheet.php
+++ qry/sheet.php
@@ -1,7 +1,7 @@
 <?php
 $file = $_REQUEST['file'];
 
-$file_url = "http://admin.yogasmoga.com/var/export/" . $file;
+$file_url = "http://staging.yogasmoga.com/var/export/" . $file;
 header('Content-Type: application/octet-stream');
 header("Content-Transfer-Encoding: Binary");
 header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\"");
