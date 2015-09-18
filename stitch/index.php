12c4e7b440d11682d5d854f21666613d77a1e2b6
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2015-09-18 22:21:35 +0530

    stitch code update

--- stitch/index.php
+++ stitch/index.php
@@ -36,7 +36,7 @@
 
     $available = 0;         // don't pick stock - available from excel, 1 means pick available
     if(isset($_GET['available'])) {
-        $available = intval(strtolower($_GET['available']));
+        $available = intval($_GET['available']);
 
         if($available!=0 || $available!=1)
             $available = 0;
