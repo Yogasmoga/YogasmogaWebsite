abb49314d701587c2ebdf20dce8d2cab44fdb5e8
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2015-10-06 15:34:08 +0530

    mailchimp subscribe monthly test

--- mailchimpsubscribemonthly.php
+++ mailchimpsubscribemonthly.php
@@ -1,111 +1,69 @@
 <?php
 require 'app/Mage.php';
-
-
 $app = Mage::app('');
-$myfile = fopen("mailchimp_list.txt", "r") or die("Unable to open file!");
-$apikey_listid = trim(fgets($myfile));
-fclose($myfile);
+umask(0);
 
-$ar = explode(",", $apikey_listid);
+$resource = Mage::getSingleton('core/resource');
+$readConnection = $resource->getConnection('core_read');
 
-$api_key = trim($ar[0]);
-$list_id = trim($ar[1]);
+$date_to_start = '2015-01-01';
+$date_to_end = '2015-07-31';
 
-$correct = isset($api_key) && isset($list_id) && (strlen($list_id) > 0) && (strlen($api_key) > 0);
+echo "Between " . $date_to_start . " and " . $date_to_end . "<br/><br/>";
 
-if ($correct) {
-    $date_to_look_start = date('Y-m-01');
-    $date_to_look_end = date('Y-m-d');
+$collection = Mage::getModel('customer/customer')->getCollection()
+    ->addAttributeToSelect('entity_id')
+    ->addAttributeToSelect('firstname')
+    ->addAttributeToSelect('lastname')
+    ->addAttributeToSelect('email')
+    ->addAttributeToFilter('created_at', array('gteq' => $date_to_start . ' 00:00:01'))
+    ->addAttributeToFilter('created_at', array('lteq' => $date_to_end . ' 23:59:59'));
 
-    echo $date_to_look_start . " , " . $date_to_look_end . "<br/><br/>";
-
-    $collection = Mage::getModel('customer/customer')->getCollection()
-        ->addAttributeToSelect('entity_id')
-        ->addAttributeToSelect('firstname')
-        ->addAttributeToSelect('lastname')
-        ->addAttributeToSelect('email')
-        ->addAttributeToFilter('created_at', array('gteq' => $date_to_look_start . ' 00:00:01'))
-        ->addAttributeToFilter('created_at', array('lteq' => $date_to_look_end . ' 23:59:59'));
-
-    if($collection && count($collection)==0){
-        echo "No records found";
-        return;
-    }
-
-    foreach ($collection as $item) {
-        $row = $item->getData();
-
-        $customerId = $row['entity_id'];
-
-        $customer = Mage::getModel('customer/customer')->load($customerId);
-
-        $customerAddress = null;
-
-        foreach ($customer->getAddresses() as $address) {
-            $customerAddress = $address->toArray();
-            break;
-        }
-
-        $country = "";
-        $state = "";
-        if($customerAddress){
-            $country = Mage::app()->getLocale()->getCountryTranslation($customerAddress['country_id']);
-            $state = $customerAddress['region'];
-        }
-
-        $batch[] = array(
-            'EMAIL' => $row['email'],
-            'FNAME' => $row['firstname'],
-            'LNAME' => $row['lastname'],
-            'STATE' => $state,
-            'COUNTRY' => $country
-        );
-    }
-
-    include("mailchimpapi/Drewm/MailChimp.php");
-
-    $mailChimp = new Drewm\MailChimp($api_key);
-
-    $i = 0;
-    foreach ($batch as $single) {
-
-        ++$i;
-
-        $email = $single["EMAIL"];
-        $fname = $single["FNAME"];
-        $lname = $single["LNAME"];
-        $state = $single["STATE"];
-        $country = $single["COUNTRY"];
-
-        $result = $mailChimp->call('lists/member-info', array(
-            'id' => $list_id,
-            'emails' => array( 0 => array('email' => $email) )
-        ));
-
-        if(isset($result["data"][0]["status"]) && $result["data"][0]["status"]=="unsubscribed"){
-            echo $result["data"][0]["email"] . " is " . $result["data"][0]["status"] . ", not changing values<br/>";
-        }
-        else if(isset($result["data"][0]["status"]) && $result["data"][0]["status"]=="subscribed"){
-            echo $result["data"][0]["email"] . " is " . $result["data"][0]["status"] . ", not changing values<br/>";
-        }
-        else{
-            $result = $mailChimp->call('lists/subscribe', array(
-                'id' => $list_id,
-                'email' => array('email' => $email),
-                'merge_vars' => array('FNAME' => $fname, 'LNAME' => $lname, 'STATE' => $state, 'COUNTRY' => $country),
-                'double_optin' => false,
-                'update_existing' => true,
-                'replace_interests' => false,
-                'send_welcome' => false,
-            ));
-
-            print_r($result);
-        }
-
-        echo "<br/><br/>";
-    }
-
-    Mage::log("Mailchimp Synchronize Task Completed at : " . date("Y-m-d h:i:s"));
+if($collection && count($collection)==0){
+    echo "No records found";
+    return;
 }
+
+foreach ($collection as $item) {
+    $row = $item->getData();
+
+    $email = $row['email'];
+
+    $query = "select email from unsubscribed_customers where email='$email'";
+    echo $query . "<br/>";
+    $rows = $readConnection->fetchAll($query);
+
+    if(isset($rows) && count($rows)>0) {
+        echo "<br/>$email is unsubscribed";
+        continue;
+    }
+
+    $customerId = $row['entity_id'];
+
+    $customer = Mage::getModel('customer/customer')->load($customerId);
+
+    $customerAddress = null;
+
+    foreach ($customer->getAddresses() as $address) {
+        $customerAddress = $address->toArray();
+        break;
+    }
+
+    $country = "";
+    $state = "";
+    if($customerAddress){
+        $country = Mage::app()->getLocale()->getCountryTranslation($customerAddress['country_id']);
+        $state = $customerAddress['region'];
+    }
+
+    $batch[] = array(
+        'EMAIL' => $row['email'],
+        'FNAME' => $row['firstname'],
+        'LNAME' => $row['lastname'],
+        'STATE' => $state,
+        'COUNTRY' => $country
+    );
+}
+
+print_r($batch);
 ?>
