483fa2a0e1bc5e3ce12929c33a72320f80031f45
Fahim Khan <kfahim59@gmail.com>
2015-12-02 13:14:53 +0530

    one to many hide in rangoli

--- rangoli/wp-content/themes/rangoli_mobile/header.php
+++ rangoli/wp-content/themes/rangoli_mobile/header.php
@@ -439,7 +439,7 @@ $logged_in = is_user_logged_in();
                                         </ul>
                                     </li>
                                     <li><a href="/women/whats-new">What&rsquo;s New</a></li>
-                                    <li><a href="/women/one-too-many">ONE 2 MANY</a></li>
+                                    <!--<li><a href="/women/one-too-many"> One 2 Many</a></li>-->
 									<li><a href="/women/super-sale">Super Sale</a></li>
                                     <li><a href="/women/additional-sizes">ADDITIONAL SIZES</a></li>
                                     <li><a href="/women/vivacity-collection">VIVACITY COLLECTION</a></li>
