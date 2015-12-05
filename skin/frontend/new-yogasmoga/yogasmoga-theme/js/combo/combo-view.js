17a0221fda6085c6497bce565ef5a24d1669686e
Ashutosh Pandey <ashutosh.options4u@gmail.com>
2015-12-05 10:20:59 +0530

    gift set view page image change on set click

--- skin/frontend/new-yogasmoga/yogasmoga-theme/js/combo/combo-view.js
+++ skin/frontend/new-yogasmoga/yogasmoga-theme/js/combo/combo-view.js
@@ -93,6 +93,8 @@ function changeProduct(product_id) {
                     jQuery(".product_price").html(allComboProducts[product_id]["price"] + "<span>" + allComboProducts[product_id]["quantity"] + " REMAINING</span>");
                     jQuery(".set_description").html(allComboProducts[product_id]["description"]);
 
+                    jQUery(".current_slider_image img").attr("src", allComboProducts[product_id]["image"] );
+
                     var data = result.data;
                     var strSets = "";
                     bundleImages = {};
