0827a5c32e522eedfce680cfe7ea8ff8cda782a1
Fahim Khan <kfahim59@gmail.com>
2015-12-09 21:31:30 +0530

    common.js update for ui-widget-overlay update

--- js/new_jquery/common.js
+++ js/new_jquery/common.js
@@ -737,7 +737,7 @@ function initializeBanner(source, str) {
 
     jQuery(".namaskar-overlay1").css("top", "94px");
     jQuery(".ui-widget-overlay").css({"top": "94px", "position": "fixed"});
-    jQuery(".ui-widget-overlay").css({top: 94});
+    jQuery(".ui-widget-overlay").css("top", "94px");
     jQuery(".header-container").css("padding-top", "25px");
     jQuery(".header-container").css("top", "0");
     jQuery("#bodycompensator").css("height", "94px");
