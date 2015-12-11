01f56879d9482f4b43bde9fc92ba6a65989be244
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2015-12-11 14:00:29 +0530

    gif set update, criteo update

--- productfeed/criteo/index.php
+++ productfeed/criteo/index.php
@@ -16,7 +16,7 @@ fwrite($fileOut, "&PROCESSTYPE=UPDATE\n");
 fwrite($fileOut, "&AID=12186878\n");
 fwrite($fileOut, "&PARAMETERS=NAME|KEYWORDS|DESCRIPTION|SKU|BUYURL|AVAILABLE|IMAGEURL|PRICE|UPC|ADVERTISERCATEGORY|MERCHANDISETYPE\n");
 
-$productCollection = Mage::getModel('catalog/product')->getCollection()->addAttributeToSelect('*')->addAttributeToFilter('status', array('eq' => Mage_Catalog_Model_Product_Status::STATUS_ENABLED));
+$productCollection = Mage::getModel('catalog/product')->getCollection()->addAttributeToSelect('*')->addAttributeToFilter('type_id', 'configurable')->addAttributeToFilter('status', array('eq' => Mage_Catalog_Model_Product_Status::STATUS_ENABLED));
 $_helper = Mage::helper('catalog/output');
 
 $count = 0;
@@ -44,75 +44,72 @@ foreach ($productCollection as $_product) {
     $advertise_category = 'yoga apparel';
     $merchandiseType = '';
 */
-    $description = trim(preg_replace('/\s+/', ' ', $description));
 
-    if (!isset($product) || !is_object($product) || !$product->getId()) {
-        ;//echo "Bad product = $name ( $sku ) <br/>";
-    } else {
-        if ($product->getTypeId() == 'configurable') {
+    if ($product->getTypeId() == 'configurable') {
 
-            $sku = $product->getSku();
-            $name = $product->getName();
-            $price = round($product->getPrice(), 2);
-            $description = $_helper->productAttribute($product, $product->getDescription(), 'description');
+        $sku = $product->getSku();
+        $name = $product->getName();
+        $price = round($product->getPrice(), 2);
+        $description = $_helper->productAttribute($product, $product->getDescription(), 'description');
+        $description = trim(preg_replace('/\s+/', ' ', $description));
 
-            $productStock = Mage::getModel('cataloginventory/stock_item')->loadByProduct($product);
-            $stock = $productStock->getQty();
-            $inStock = $productStock->getIsInStock();
-            if ($stock <= 0 || !$inStock)
-                $available = 'NO';
-            else
-                $available = 'YES';
+        $productStock = Mage::getModel('cataloginventory/stock_item')->loadByProduct($product);
+        $stock = $productStock->getQty();
+        $inStock = $productStock->getIsInStock();
+        if ($stock <= 0 || !$inStock)
+            $available = 'NO';
+        else
+            $available = 'YES';
 
-            $keyword = '';
-            $buy_url = '';
-            $image_url = '';
-            $advertise_category = 'yoga apparel';
-            $merchandiseType = '';
+        $keyword = '';
+        $buy_url = '';
+        $image_url = '';
+        $advertise_category = 'yoga apparel';
+        $merchandiseType = '';
 
-            $forHidden = $product->getAttributeText('hidden_product');
-            if (isset($forHidden) && strtolower($forHidden) == "yes")
-                continue;
+        $forHidden = $product->getAttributeText('hidden_product');
+        if (isset($forHidden) && strtolower($forHidden) == "yes")
+            continue;
 
-            $categoryIds = $product->getCategoryIds();
+        $categoryIds = $product->getCategoryIds();
 
-            if (isset($categoryIds) && is_array($categoryIds) && count($categoryIds) > 0) {
-                foreach ($categoryIds as $id) {
+        if (isset($categoryIds) && is_array($categoryIds) && count($categoryIds) > 0) {
+            foreach ($categoryIds as $id) {
 
-                    if (intval($id) === 43 || intval($id) === 12 || intval($id) === 8) {
-                        $merchandiseType = "Non-commissionable Items";
-                        break;
-                    }
-                }
-            } else
-                continue;
-
-            unset($categoryIds);
-
-            $buy_url = $product->getUrlInStore();
-            $keywords = $product->getMetaKeyword();
-
-            $images = Mage::getModel('catalog/product')->load($product->getId())->getMediaGalleryImages();
-
-            if (isset($images) && count($images) > 0) {
-                foreach ($images as $image) {
-
-                    $imgdata = json_decode(trim($image->getLabel()), true);
-                    if (isset($imgdata['type']) && $imgdata['type'] == 'product image') {
-                        $image_url = (string)Mage::helper('catalog/image')->init($product, 'thumbnail', $image->getFile())->constrainOnly(TRUE)->keepAspectRatio(TRUE)->keepFrame(FALSE)->resize(771, 700)->setQuality(91);
-                        break;
-                    }
+                if (intval($id) === 43 || intval($id) === 12 || intval($id) === 8) {
+                    $merchandiseType = "Non-commissionable Items";
+                    break;
                 }
             }
+        } else
+            continue;
 
-            $sku = str_replace('.', '-', $sku);
-            $upc = $sku;
+        unset($categoryIds);
 
-            $data = "$name|$keywords|$description|$sku|$buy_url|$available|$image_url|$price|$upc|$advertise_category|$merchandiseType\n";
+        $buy_url = $product->getUrlInStore();
+        $keywords = $product->getMetaKeyword();
 
-            fwrite($fileOut, $data);
+        $images = Mage::getModel('catalog/product')->load($product->getId())->getMediaGalleryImages();
+
+        if (isset($images) && count($images) > 0) {
+            foreach ($images as $image) {
+
+                $imgdata = json_decode(trim($image->getLabel()), true);
+                if (isset($imgdata['type']) && $imgdata['type'] == 'product image') {
+                    $image_url = (string)Mage::helper('catalog/image')->init($product, 'thumbnail', $image->getFile())->constrainOnly(TRUE)->keepAspectRatio(TRUE)->keepFrame(FALSE)->resize(771, 700)->setQuality(91);
+                    break;
+                }
+            }
         }
+
+        $sku = str_replace('.', '-', $sku);
+        $upc = $sku;
+
+        $data = "$name|$keywords|$description|$sku|$buy_url|$available|$image_url|$price|$upc|$advertise_category|$merchandiseType\n";
+
+        fwrite($fileOut, $data);
     }
+
 }
 
 fclose($fileOut);
