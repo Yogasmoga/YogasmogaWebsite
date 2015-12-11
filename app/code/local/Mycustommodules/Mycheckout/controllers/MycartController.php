beb43e4f9457bd3c1bcd5a3cd1ce04c3f9ff0428
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2015-12-11 19:09:27 +0530

    shopping cart count fix

--- app/code/local/Mycustommodules/Mycheckout/controllers/MycartController.php
+++ app/code/local/Mycustommodules/Mycheckout/controllers/MycartController.php
@@ -607,10 +607,12 @@ class Mycustommodules_Mycheckout_MycartController extends Mage_Core_Controller_F
 
                 if(isset($product_type) && $product_type=="gift-bundled")
                     continue;
+
+                $count = $count + $item->getQty();
             }
             else if(Mage::getModel('catalog/product')->load($item->getProductId())->getTypeID() == "simple")
                 ;
-            else 
+            else
                 ++$count;
         }
 
