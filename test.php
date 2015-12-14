1a372719aef8a7f3e88edc40c52e9624a9daa8d2
Fahim Khan <kfahim59@gmail.com>
2015-12-14 13:22:06 +0530

    customer reward points

--- test.php
+++ test.php
@@ -3,19 +3,36 @@
 
     Mage::app();
 
-    //$customerIds = array(12516,20424,21335,16130,20423,21346,21347);
+    
 	$customerIds = Mage::getModel('customer/customer')->getCollection();
 	
-	//$customerIds = array(14893);
+	header('Content-Type: text/csv; charset=utf-8');
+	header('Content-Disposition: attachment; filename=data.csv');
+
+
+	$output = fopen('php://output', 'w');
+
+	fputcsv($output, array('customer id','customer email', 'smogi bucks'));
+	
+	
+	
     foreach($customerIds as $customer) {
 		
 		$customerId = $customer->getId();
+		$customerEmail = $customer->getEmail();
 		$store_id = Mage::app()->getStore()->getId();
         $reward_model = Mage::getModel('rewardpoints/stats');
 		$points = $reward_model->getPointsCurrent($customerId, $store_id);
 		if($points > 0){
-		echo $customerId."--Points = $points<br/>";
+		$smogiBucks = $points;
         }
+		
+		$rows = array(
+					'customer id'=>$customerId,
+					'customer email'=>$customerEmail,
+					'smogi bucks'=>$smogiBucks
+					);
+		
 		//echo "<br/>Removing<br/>";
         //$reward_model->setPointsSpent($points);
         //$reward_model->setCustomerId($customerId);
@@ -26,5 +43,12 @@
         //$points = $reward_model->getPointsCurrent($customerId, $store_id);
         //echo "Points now = $points";
         //echo "<hr/>";
+		fputcsv($output, $rows);	
+		
     }
 ?>
+
+
+
+
+
