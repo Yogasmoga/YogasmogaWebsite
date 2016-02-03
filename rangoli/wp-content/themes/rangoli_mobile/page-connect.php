b0bdb081d761a70549f38857666c0360699d85df
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2016-02-03 19:23:56 +0530

    add invite link url in page-connect.php in rangoli mobile theme

--- rangoli/wp-content/themes/rangoli_mobile/page-connect.php
+++ rangoli/wp-content/themes/rangoli_mobile/page-connect.php
@@ -12,7 +12,7 @@ get_header();
           Your invite link
     </div>
     <div class="invite_link" style="background: #555; height:50px;">
-    <p <?php if(!is_user_logged_in()){echo "style='background:rgba(85,85,85,0.95);'";} else { echo "class='user-color-shade'";} ?> style="height: 50px;"><i class="link"></i> <span class="copy_link" data-clipboard-text="">Copy link</span></p>
+    <p <?php if(!is_user_logged_in()){echo "style='background:rgba(85,85,85,0.95);'";} else { echo "class='user-color-shade'";} ?> style="height: 50px;"><i class="link">http://rango.li/1JkCPWc</i> <span class="copy_link" data-clipboard-text="">Copy link</span></p>
     </div>
 
     <div style="text-align: center; padding-bottom: 25px;">
