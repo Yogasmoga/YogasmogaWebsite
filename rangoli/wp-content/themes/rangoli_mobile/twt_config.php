85c1e8d1a208ac185aea22ec5581afbe5f738d8f
Rajan Kumar <info@yogasmoga.com>
2016-02-03 19:10:02 +0530

    rangoli mobile twitter correction

--- rangoli/wp-content/themes/rangoli_mobile/twt_config.php
+++ rangoli/wp-content/themes/rangoli_mobile/twt_config.php
@@ -3,8 +3,8 @@ define('CONSUMER_KEY', 'Lqix1mk4xXqPoirmKEq7QcazH');
 define('CONSUMER_SECRET','TKt8zuEdBXCDa7euGhbXUxdPUmF6og4XC79Xrd1aVHSUx8GAeW');
 define('ACCESS_TOKEN','315003413-okgplYaH5GVt2BPeoztvqe8gX9L5wgkbVwMpAsDv');
 define('ACCESS_TOKEN_SECRET','nwnhZpwfFwLpp0EQJOfCdOpL0Rq8qTKsyi9JAzrYgveXI');
-define('REFERRAL_URL', "http://staging.yogasmoga.com/profile/manage/referralurl");
-define('OAUTH_CALLBACK', "http://staging.yogasmoga.com/rangoli/wp-content/themes/rangoli/twt_callback.php");
+define('REFERRAL_URL', "http://yogasmoga.com/profile/manage/referralurl");
+define('OAUTH_CALLBACK', "http://yogasmoga.com/rangoli/wp-content/themes/rangoli/twt_callback.php");
 /*
 define('CONSUMER_KEY', '6XSZy2EOadGs68HJIMCNRQ3hZ');
 define('CONSUMER_SECRET','y2oNIGPMWjIiK0YYQovraEpGqrsJI0QPkGwoT4GHPn81SxxCID');
