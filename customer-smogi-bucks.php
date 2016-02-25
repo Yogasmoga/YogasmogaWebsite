a77ee6d723850555df792abb5b365768575b8608
Rajan Kumar <info@yogasmoga.com>
2016-02-25 13:56:39 +0530

    customer smogi bucks script

--- customer-smogi-bucks.php
+++ customer-smogi-bucks.php
@@ -5,21 +5,32 @@
 
 	$email = $_GET['email'];
 
-	$customer = Mage::getModel("customer/customer");
-	$customer->setWebsiteId(Mage::app()->getWebsite()->getId());
-	$customer->loadByEmail($email);
+	if(isset($email)) {
+		$customer = Mage::getModel("customer/customer");
+		$customer->setWebsiteId(Mage::app()->getWebsite()->getId());
+		$customer->loadByEmail($email);
 
-	$customerId = $customer->getId();
+		if (isset($customer)) {
+			$customerId = $customer->getId();
+			$store_id = Mage::app()->getStore()->getId();
 
-	$store_id = Mage::app()->getStore()->getId();
+			$reward_model = Mage::getModel('rewardpoints/stats');
+			$points = $reward_model->getPointsCurrent($customerId, $store_id);
 
-	$reward_model = Mage::getModel('rewardpoints/stats');
-	$points = $reward_model->getPointsCurrent($customerId, $store_id);
+			echo json_encode(array('points' => $points, "message" => "Customer found"));
 
-	echo json_encode(array('points' => $points));
+			if (false) {
+				//$link = Mage::getBaseUrl().'api/soap/?wsdl';
+				$link = Mage::getBaseUrl() . "api/v2_soap/?wsdl";
+				$proxy = new SoapClient($link);
+				var_dump($proxy);
+				//$sessionId = $proxy->login('api_login', 'j2t_apikey');
+
+				//var_dump($proxy->call($sessionId, 'j2trewardapi.info', array($customerId, $storeId)));
+			}
+		} else
+			echo json_encode(array('points' => 0, "message" => "Customer not found"));
+	}
+	else
+		echo json_encode(array('points' => 0, "message" => "No email provided"));
 ?>
-
-
-
-
-
