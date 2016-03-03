bd5d2db7432dc84b027e1fe37e9af1cd1da6857e
Ashutosh Pandey <ashutosh.options4u@gmail.com>
2016-03-03 14:25:08 +0530

    facebook lead file update

--- facebook_lead.php
+++ facebook_lead.php
@@ -5,3 +5,6 @@ $token = $_REQUEST['hub_verify_token'];
 if($token=='abc123'){
     echo $challenge;
 }
+
+$input = json_decode(file_get_contents('php://input'), true);
+error_log(print_r($input, true));
