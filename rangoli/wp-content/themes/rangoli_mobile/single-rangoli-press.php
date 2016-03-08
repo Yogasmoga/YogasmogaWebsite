7d655bac19001a956b4da8d02bbabaaf88004da4
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2016-03-08 17:27:57 +0530

    files modified for rangoli press for desktop and mobile

--- rangoli/wp-content/themes/rangoli_mobile/single-rangoli-press.php
+++ rangoli/wp-content/themes/rangoli_mobile/single-rangoli-press.php
@@ -56,7 +56,7 @@ $banner_img_url=wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ),
             }
             ?>
             <div class="author_picture press-icon"
-                 style="background: url('<?php echo(types_render_field( "contributing-website-icon", array( 'raw' => true) )); ?>') no-repeat;
+                 style="background: url('<?php echo(types_render_field( "contributing-website-icon-mobile", array( 'raw' => true) )); ?>') no-repeat;
                      background-position: center center; background-size: cover;
                  "><a href="<?php echo(types_render_field( "contributing-website-url", array( 'raw' => true) )); ?>"><?php echo(types_render_field( "contributing-website", array( 'raw' => true) ));?></a></div>
             
