a942e799a778cfe0454c2e87d20fe23adb7df596
Rajan Kumar <info@yogasmoga.com>
2016-03-16 14:15:57 +0530

    customer attribute script

--- customer_attribute_add.php
+++ customer_attribute_add.php
@@ -5,8 +5,8 @@ Mage::app();
 $installer = Mage::getResourceModel('customer/setup', 'customer_setup');
 $installer->startSetup();
 
-$installer->addAttribute('customer','location_city', array(
-    'label'             => 'Source City',
+$installer->addAttribute('customer','location_state', array(
+    'label'             => 'Source State',
     'type'              => 'text',    //backend_type
     'input'             => 'text', //frontend_input
     'global'            =>  Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
@@ -19,7 +19,7 @@ $installer->addAttribute('customer','location_city', array(
 ));
 
 Mage::getSingleton('eav/config')
-    ->getAttribute('customer', 'location_city')
+    ->getAttribute('customer', 'location_state')
     ->setData('used_in_forms', array(
         'customer_account_create', 'customer_account_edit', 'customer_address_edit',
         'checkout_onepage_register', 'checkout_onepage_register_guest', 'checkout_onepage_billing_address',
