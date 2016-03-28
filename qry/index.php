85a0896eaffd5a4616924381a940ac127059ecdd
Rajan Kumar <info@yogasmoga.com>
2016-03-28 13:42:58 +0530

    qry/index.php update

--- qry/index.php
+++ qry/index.php
@@ -3,8 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Manage Reports</title>
-    <link href="style.css" type="text/css" rel="stylesheet"/>
-    <link href="http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+
 </head>
 <body>
 <header><h1>Manage Reports</h1></header>
