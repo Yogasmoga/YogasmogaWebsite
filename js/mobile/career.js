085c0896ba82a03d5f0f584a434cd654e49fbcf1
Rajan Kumar <info@yogasmoga.com>
2016-04-06 15:43:39 +0530

    mobile career page update

---
+++ js/mobile/career.js
@@ -0,0 +1,113 @@
+jQuery.noConflict();
+var type = window.location.hash.substr(1);
+jQuery(".sign-in-box h1").html("CAREER");
+jQuery(document).ready(function ($) {
+
+ var questionPos = type.indexOf("?");
+ if (questionPos > -1)
+  type = type.substring(0, questionPos);
+
+ if (type == "") {
+  type = "career1";
+ }
+ $(".career-page").hide();
+ $(".career-page#" + type).show();
+ /******* code for accordian ***/
+ $(".career-page li .toggle").click(function (e) {
+  e.preventDefault();
+  $(this).parent().siblings().find(".toggle").removeClass('active');
+  $(this).toggleClass('active');
+  $(this).parent('li').toggleClass('active');
+  $(this).parent('li').find(".answer_content").slideUp('fast');
+  $(this).parent('li.active').find(".answer_content").slideDown('fast');
+ });
+ /******* code for accordian ***/
+ $(".sub-nav li a").click(function (e) {
+  e.preventDefault();
+  var target = $(this).attr('rel');
+  $("#"+target).show().siblings(".career-page").hide();
+
+ });
+
+ /*===========ajax career data========*/
+ if(window.location.href.indexOf('https://') >= 0)
+  _usesecureurl = true;
+ else
+  _usesecureurl = false;
+ var url = homeUrl;
+ if(_usesecureurl)
+  url = securehomeUrl;
+
+ $('.sub-nav li a').click(function(e){
+  e.preventDefault();
+  var state = $(this).attr('rel');
+  jQuery.ajax({
+   type: 'POST',
+   url: url+ 'career/index/mobilestatejobs',
+   data: {'state': state},
+   beforeSend:function (data) {
+    //jQuery("#content-loader").show();
+   },
+   success: function (data) {
+    //jQuery("#content-loader").hide();
+    jQuery(".career-page").html(data);
+    /******* code for accordian ***/
+    $(".career-page li .toggle").click(function (e) {
+     e.preventDefault();
+     $(this).parent().siblings().find(".toggle").removeClass('active');
+     $(this).toggleClass('active');
+     $(this).parent('li').toggleClass('active');
+     $(this).parent('li').find(".answer_content").slideUp('fast');
+     $(this).parent('li.active').find(".answer_content").slideDown('fast');
+    });
+    /******* code for accordian ***/
+    $(".sub-nav li a").click(function (e) {
+     e.preventDefault();
+     var target = $(this).attr('rel');
+     $("#"+target).show().siblings(".career-page").hide();
+
+    });
+
+   },
+   error: function() {
+    alert('Sorry for inconvinious try after some time');
+   },
+
+  });
+ });
+
+ jQuery.ajax({
+  type: 'POST',
+  url: url+ 'career/index/mobiledefaultstatejobs',
+  data: {'state': 1},
+  beforeSend:function (data) {
+   //jQuery("#content-loader").show();
+  },
+  success: function (data) {
+   //jQuery("#content-loader").hide();
+   jQuery(".career-page").html(data);
+   /******* code for accordian ***/
+   $(".career-page li .toggle").click(function (e) {
+    e.preventDefault();
+    $(this).parent().siblings().find(".toggle").removeClass('active');
+    $(this).toggleClass('active');
+    $(this).parent('li').toggleClass('active');
+    $(this).parent('li').find(".answer_content").slideUp('fast');
+    $(this).parent('li.active').find(".answer_content").slideDown('fast');
+   });
+   /******* code for accordian ***/
+   $(".sub-nav li a").click(function (e) {
+    e.preventDefault();
+    var target = $(this).attr('rel');
+    $("#"+target).show().siblings(".career-page").hide();
+
+   });
+
+  },
+  error: function() {
+   alert('Sorry for inconvinious try after some time');
+  },
+
+ });
+
+});
