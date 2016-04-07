0bb1df09ed5f121eab9126aff43c3e453fa2e8be
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2016-04-07 13:00:54 +0530

    career.js updated in career mobile

--- js/mobile/career.js
+++ js/mobile/career.js
@@ -13,21 +13,21 @@ jQuery(document).ready(function ($) {
  $(".career-page").hide();
  $(".career-page#" + type).show();
  /******* code for accordian ***/
- $(".career-page li .toggle").click(function (e) {
+/*  $(".career-page li .toggle").click(function (e) {
   e.preventDefault();
   $(this).parent().siblings().find(".toggle").removeClass('active');
   $(this).toggleClass('active');
   $(this).parent('li').toggleClass('active');
   $(this).parent('li').find(".answer_content").slideUp('fast');
   $(this).parent('li.active').find(".answer_content").slideDown('fast');
- });
+ }); */
  /******* code for accordian ***/
- $(".sub-nav li a").click(function (e) {
+ /* $(".sub-nav li a").click(function (e) {
   e.preventDefault();
   var target = $(this).attr('rel');
   $("#"+target).show().siblings(".career-page").hide();
 
- });
+ }); */
 
  /*===========ajax career data========*/
  if(window.location.href.indexOf('https://') >= 0)
