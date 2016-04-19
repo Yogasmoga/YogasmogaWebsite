538f35b5a2687f29b64bb0783fefcd428e84c85a
Shivaji Chauhan <Shivaji Chauhan>
2016-04-19 13:11:03 +0530

    test time for registration reward

--- testtime.php
+++ testtime.php
@@ -2,12 +2,14 @@
 require_once 'app/Mage.php';
 Mage::app();umask(0);
 $post = array();
-
+Mage::getConfig()->init();
+$mod  = Mage::getConfig()->loadModules('Rewardpoints');
+$reward_model = Mage::getModel('rewardpoints/stats');
 //v.2.0.0
 $add_delay = 0;
 if ($delay = Mage::getStoreConfig('rewardpoints/default/points_delay', $store_id) && $force_nodelay){
      if (is_numeric($delay)){
-         $post['date_start'] = $this->getResource()->formatDate(mktime(0, 0, 0, date("m"), date("d")+$delay, date("Y")));
+         $post['date_start'] = $reward_model->getResource()->formatDate(mktime(0, 0, 0, date("m"), date("d")+$delay, date("Y")));
          $add_delay = $delay;
       }
       }
@@ -15,9 +17,9 @@ if ($delay = Mage::getStoreConfig('rewardpoints/default/points_delay', $store_id
      if ($duration = Mage::getStoreConfig('rewardpoints/default/points_duration', $store_id)){
          if (is_numeric($duration)){
           if (!isset($post['date_start'])){
-          $post['date_start'] = $this->getResource()->formatDate(time());
+          $post['date_start'] = $reward_model->getResource()->formatDate(time());
           }
-          $post['date_end'] = $this->getResource()->formatDate(mktime(0, 0, 0, date("m"), date("d")+$duration+$add_delay, date("Y")));
+          $post['date_end'] = $reward_model->getResource()->formatDate(mktime(0, 0, 0, date("m"), date("d")+$duration+$add_delay, date("Y")));
         }
       }
 var_dump($post);
