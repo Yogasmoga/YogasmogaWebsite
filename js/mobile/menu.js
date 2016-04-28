f4876a77c25badd79f386d9f28883a5bca0cdaf5
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2016-04-28 15:30:14 +0530

    menu.js and iphone-new.css updated for mobile

--- js/mobile/menu.js
+++ js/mobile/menu.js
@@ -242,9 +242,8 @@ jQuery(document).ready(function($){
     });
     $("p.apply_promo_label > span").click(function(){
         $("#error_msg_coupon").html("");
-		$(".apply_promo_code_div").show();
+        $(".apply_promo_code_div").show();
         $(".apply_smogi_code_div").hide();
-		
     });
 
     $("p.apply_smogi_label > span").click(function(){
