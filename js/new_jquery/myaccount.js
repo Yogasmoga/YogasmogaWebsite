b0b78a633a80c981882b036d733d1a931eb3d850
Shivaji Chauhan <Shivaji Chauhan>
2016-05-16 14:39:16 +0530

    desktop myaccount zip validation

--- js/new_jquery/myaccount.js
+++ js/new_jquery/myaccount.js
@@ -452,12 +452,12 @@ function validateAddressForm()
 {
     unsetAllError(jQuery("#address-form"));
     var flag = validatefields(jQuery("#address-form"));
-    if(jQuery("zip").val() != "")
+    if(jQuery("#zip").val() != "")
     {
-        if(!validateZip(jQuery("zip").val()))
+        if(!isValidPostalCode(jQuery("#zip").val(),jQuery("#country").val()) || jQuery("#zip").val() == 'Zip/Postal Code' )
         {
             flag = false;
-            setOnError(jQuery("zip"), "Invalid Zip Code.");
+            setOnError(jQuery("#zip"), "Invalid Zip Code.");
         }
     }
     return flag;
