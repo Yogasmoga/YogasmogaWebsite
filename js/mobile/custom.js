2b308c4e0c2c9218fe99289c58c882481f1069ae
Shivaji Chauhan <Shivaji Chauhan>
2016-05-16 12:59:38 +0530

    mobile myaccount zip validation

--- js/mobile/custom.js
+++ js/mobile/custom.js
@@ -4,3 +4,15 @@ jQuery(window).load(function() {
 	animation: "slide"
   });
 });
+
+
+function isValidPostalCode(zip, countryCode) {
+    switch (countryCode) { 
+        case "US":
+            re = /^([A-z0-9]{5,10})$/;
+            break;
+        default:
+            var re = /(^[A-z0-9]{2,10}([\s]{0,1}|[\-]{0,1})[A-z0-9]{2,10}$)/;
+    }
+    return re.test(zip);
+}
--- js/mobile/mycheckout.js
+++ js/mobile/mycheckout.js
@@ -190,13 +190,3 @@ function validatePaymentForm()
     return flag;
 }
 
-function isValidPostalCode(zip, countryCode) {
-    switch (countryCode) { 
-        case "US":
-            re = /^([A-z0-9]{5,10})$/;
-            break;
-        default:
-            var re = /(^[A-z0-9]{2,10}([\s]{0,1}|[\-]{0,1})[A-z0-9]{2,10}$)/;
-    }
-    return re.test(zip);
-}
