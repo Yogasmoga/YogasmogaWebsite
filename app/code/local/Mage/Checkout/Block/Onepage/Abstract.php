91a27524e26f15ee97bce6ad838aaa3ebe900458
info@yogasmoga.com <info@yogasmoga.com>
2016-05-27 16:30:56 +0530

    Dropdown address selection update on mobile checkout

--- app/code/local/Mage/Checkout/Block/Onepage/Abstract.php
+++ app/code/local/Mage/Checkout/Block/Onepage/Abstract.php
@@ -144,6 +144,44 @@ abstract class Mage_Checkout_Block_Onepage_Abstract extends Mage_Core_Block_Temp
         }
         return '';
     }
+    // Added by Fahim Khan For Mobile Checkout DropDown.
+    public function getAddressesMobileSelect($type)
+    {
+        if ($this->isCustomerLoggedIn()) {
+            $options = array();
+            foreach ($this->getCustomer()->getAddresses() as $address) {
+                $options[] = array(
+                    'value' => $address->getId(),
+                    'label' => $address->format('add_mobile')
+                );
+            }
+
+            $addressId = $this->getAddress()->getCustomerAddressId();
+            if (empty($addressId)) {
+                if ($type=='billing') {
+                    $address = $this->getCustomer()->getPrimaryBillingAddress();
+                } else {
+                    $address = $this->getCustomer()->getPrimaryShippingAddress();
+                }
+                if ($address) {
+                    $addressId = $address->getId();
+                }
+            }
+
+            $select = $this->getLayout()->createBlock('core/html_select')
+                ->setName($type.'_address_id')
+                ->setId($type.'-address-select')
+                ->setClass('address-select')
+                ->setExtraParams('onchange="'.$type.'.newAddress(!this.value)"')
+                ->setValue($addressId)
+                ->setOptions($options);
+
+            $select->addOption('', Mage::helper('checkout')->__('New Address'));
+
+            return $select->getHtml();
+        }
+        return '';
+    }
 
     public function getCountryHtmlSelect($type)
     {
