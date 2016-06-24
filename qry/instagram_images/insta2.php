f9706b98750422dc4c6759ee3ef3896eef667a87
Ashutosh Pandey <ashutosh.pandey@yogasmoga.com>
2016-06-24 17:27:15 +0530

    insta2.php updated and instagra.class.php renamed

--- qry/instagram_images/insta2.php
+++ qry/instagram_images/insta2.php
@@ -1,7 +1,7 @@
 <ul id="instagram_widget">
 	<?php       
 	// Download: https://github.com/cosenary/Instagram-PHP-API/blob/master/instagram.class.php
-	require_once 'http://staging.yogasmoga.com/qry/instagram_images/instagram.class.php';
+	require_once 'http://staging.yogasmoga.com/qry/instagram_images/instagram_class.php';
 	// Register at http://instagram.com/developer/ and replace client_id with your own
 	$instagram = new Instagram('999f9f95809944558d0151f0fe93daed');
 	$tag = 'yogasmoga';
@@ -14,5 +14,7 @@
 		echo '<p class="instagram_likes"><span class="likes_count">'.$data->likes->count.'</span></p>';
         echo '</li>';
     }
+	
+	
 	?>
 </ul>
