af6ade47bc79c2bfb0dcb20e08622ebb6414ede5
info@yogasmoga.com <info@yogasmoga.com>
2016-07-28 18:55:13 +0530

    wp-config.php update

--- rangoli/wp-config.php
+++ rangoli/wp-config.php
@@ -19,10 +19,10 @@
 define('DB_NAME', 'staging');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'ys');
 
 /** MySQL database password */
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', 'india');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
