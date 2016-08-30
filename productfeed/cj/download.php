681226c23d2d6d20d8f9535cb34cfe88d1955ec8
info@yogasmoga.com <info@yogasmoga.com>
2016-08-30 12:44:22 +0530

    cj productfeed and mailchim-popup-merge file update

--- productfeed/cj/download.php
+++ productfeed/cj/download.php
@@ -1,6 +1,7 @@
 <?php
-$root = "admin.yogasmoga.com";
-$file_url = "http://$root/var/productfeed/result.txt";
+//$root = "admin.yogasmoga.com";
+$root = "ysmaster.dev";
+$file_url = "http://$root/var/productfeed/cjresult.txt";
 header('Content-Type: application/octet-stream');
 header("Content-Transfer-Encoding: Binary");
 header("Content-disposition: attachment; filename=\"" . basename($file_url) . "\"");
---
+++ qry/mailchimp-popup-merge.php
@@ -0,0 +1,135 @@
+<?php
+//exit;
+ini_set('max_execution_time',1800);
+require_once '../app/Mage.php';
+
+$app = Mage::app('');
+umask(0);
+/*
+    $csvFile = file('mailchimp_export.csv');
+        $data = array();
+        $emails = array();
+    foreach ($csvFile as $line) {
+            $data[] = str_getcsv($line);
+    }
+    for($i=1;$i <count($data);$i++){
+        array_push($emails,$data[$i][0]);
+
+    }
+*/
+
+$api_key = 'e49184c3866b4d458797fdffe11f22d8-us3';
+$list_id = "40611aba4e"; //New List
+
+
+
+    $date_to_look_start = date('Y-m-d', strtotime('-15 day', strtotime(date('Y-m-d'))));
+    $date_to_look_end = date('Y-m-d', strtotime(date('Y-m-d')));
+
+    echo $date_to_look_start . " , " . $date_to_look_end . "\n\n";
+
+    $collection = Mage::getModel('customer/customer')->getCollection()
+        ->addAttributeToSelect('entity_id')
+        ->addAttributeToSelect('firstname')
+        ->addAttributeToSelect('lastname')
+        ->addAttributeToSelect('gender')
+        ->addAttributeToSelect('email')
+        ->addAttributeToFilter('created_at', array('gteq' => $date_to_look_start . ' 00:00:01'))
+        ->addAttributeToFilter('created_at', array('lteq' => $date_to_look_end . ' 23:59:59'));
+    //echo "<pre/>";
+    //print_r($collection->getData('email'));
+    echo count($collection).'<br/>';
+    if($collection && count($collection)==0){
+        echo "No records found";
+        return;
+    }
+
+    foreach ($collection as $item) {
+                $row = $item->getData();
+
+                $customerId = $row['entity_id'];
+                /*
+                    $customer = Mage::getModel('customer/customer')->load($customerId);
+
+                    $customerAddress = null;
+
+                    foreach ($customer->getAddresses() as $address) {
+                    $customerAddress = $address->toArray();
+                    break;
+                    }
+
+                    $country = "";
+                    $state = "";
+                    if($customerAddress){
+                            $country = Mage::app()->getLocale()->getCountryTranslation($customerAddress['country_id']);
+                            $state = $customerAddress['region'];
+                    }
+                */
+
+            $batch[] = array(
+                'EMAIL' => $row['email'],
+                'FNAME' => $row['firstname'],
+                'LNAME' => $row['lastname'],
+              //  'STATE' => $state,
+              //  'COUNTRY' => $country,
+                'CUSTOMERID'=> $customerId,
+                'GENDER' => $row['gender']
+            );
+    }
+
+    include("../mailchimpapi/Drewm/MailChimp.php");
+
+    $mailChimp = new Drewm\MailChimp($api_key);
+
+    $i = 0;
+    foreach ($batch as $single) {
+
+            ++$i;
+
+            $email = $single["EMAIL"];
+            $fname = $single["FNAME"];
+            $lname = $single["LNAME"];
+            $custId = $single['CUSTOMERID'];
+           //$state = $single["STATE"];
+           // $country = $single["COUNTRY"];
+            $gender = $single["GENDER"];
+
+               if(isset($gender) && strlen(trim($gender))>0){
+
+                    if($gender==1)
+                        $gender = "MALE";
+                    else
+                        $gender = "FEMALE";
+                }
+
+            $result = $mailChimp->call('lists/member-info', array(
+                'id' => $list_id,
+                'emails' => array( 0 => array('email' => $email) )
+            ));
+
+            if($custId == 34325){
+
+                /*
+                if(isset($result["data"][0]["status"]) && $result["data"][0]["status"]=="subscribed"){
+
+                        $result = $mailChimp->call('lists/subscribe', array(
+                            'id' => $list_id,
+                            'email' => array('email' => $email),
+                            'merge_vars' => array('FNAME' => $fname, 'LNAME' => $lname, 'GENDER' => $gender, 'MAILSOURCE' => 'Magento','MMERGE19'=>'Yes'),
+                            'double_optin' => false,
+                            'update_existing' => true,
+                            'replace_interests' => false,
+                            'send_welcome' => false,
+                        ));
+
+                        echo $email . " , $gender \n";
+
+                }*/
+                echo $email . " , $gender \n";
+            }
+
+    }
+
+    //echo "\nMailchimp Synchronize Task Completed at : " . date("Y-m-d h:i:s");
+
+?>
