3b2f145925762b15217ff3d5073b813cc593fa29
info@yogasmoga.com <info@yogasmoga.com>
2016-08-30 17:57:27 +0530

    cron emai test

---
+++ cron-test-demo.php
@@ -0,0 +1,19 @@
+<?php
+// Send email.
+require_once 'app/Mage.php';
+Mage::app();
+umask(0);
+
+$email = 'fahim.khan@yogasmoga.com';
+$templateId = "share_smogi_bucks";
+
+$emailTemplate = Mage::getModel('core/email_template')->loadByCode($templateId);
+$storeId = Mage::app()->getStore()->getId();
+$vars = array( 'email'=> $email);
+
+$emailTemplate->getProcessedTemplate($vars);
+$emailTemplate->setSenderEmail(Mage::getStoreConfig('trans_email/ident_general/email', $storeId));
+$emailTemplate->setSenderName(Mage::getStoreConfig('trans_email/ident_general/name', $storeId));
+$emailTemplate->send($email, $vars);
+
+echo "Thank You";
