8f43e1f617fe006ce09c6a3393caf1f9a2a88627
Ankur Shukla <ankur.shukla@yogasmoga.com>
2016-09-07 17:48:52 +0530

    product.js update

--- js/new_jquery/product.js
+++ js/new_jquery/product.js
@@ -146,12 +146,12 @@ jQuery(document).ready(function ($) {
         var xyzinterval = function () {
             if (current >= $imgs.length)
                 current = 0;
-           /* $imgs.eq(current++).css("visibility", "visible").fadeIn(function () {
+            $imgs.eq(current++).css("visibility", "visible").fadeIn(function () {
                 $(this).delay(500).fadeOut(xyzinterval);
-            });*/
-			$imgs.eq(current++).css("visibility", "visible").show('0',function () {
-                $(this).delay(500).hide();
             });
+			/*$imgs.eq(current++).css("visibility", "visible").show('0',function () {
+                $(this).delay(500).hide();
+            });*/
         };
         xyzinterval();
     }, function () {
