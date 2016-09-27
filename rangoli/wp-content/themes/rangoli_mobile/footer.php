1866054be42cb65497fda5a4ba1d8d72616402a5
sanjay <sanjay.kumar@yogasmoga.com>
2016-09-27 14:43:13 +0530

    magento signup popup update in rangoli

--- rangoli/wp-content/themes/rangoli_mobile/footer.php
+++ rangoli/wp-content/themes/rangoli_mobile/footer.php
@@ -312,6 +312,9 @@ if(!is_user_logged_in()  && is_home()) {
 				jQuery('.no-thanks,.already_signed_up').click(function(){
 					jQuery('.popup').removeClass('signup_pop');
 				});
+				if(jQuery('.signup_popup').fadeIn()){
+					jQuery('.popup').addClass('signup_pop');
+				}
 			
 		$("#popup-mailsignup").submit(function(event){
 		event.preventDefault();
