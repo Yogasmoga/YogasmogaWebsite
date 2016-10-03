e361b60eee0d27b69de5abbdf440165f16e12c45
sanjay <sanjay.kumar@yogasmoga.com>
2016-10-03 17:56:25 +0530

    new design update

--- js/new_jquery/signing.js
+++ js/new_jquery/signing.js
@@ -726,7 +726,8 @@ function doWordpressLogin(email, password, first_name, last_name, customer_id) {
 				if( winScrollTop > filterOffsetTop){
 
 				jQuery("#div_sizes,#div_cats").addClass("fixed_top");
-				jQuery("#div_sizes.fixed_top,#div_cats.fixed_top").css("top","69px");
+				jQuery("#div_sizes.fixed_top,#div_cats.fixed_top").css("top","58px");
+				//jQuery("#div_sizes.fixed_top,#div_cats.fixed_top").css("top","69px");				
 				//jQuery("#div_sizes.fixed_top,#div_cats.fixed_top").css("top","94px");
 				}else{
 				jQuery("#div_sizes,#div_cats").removeClass("fixed_top");
