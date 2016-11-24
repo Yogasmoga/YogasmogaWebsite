8871b84f42de3354adbab4157f903a27a604cf74
info@yogasmoga.com <info@yogasmoga.com>
2016-11-24 18:45:33 +0530

    update

--- orderData4shipstation.php
+++ orderData4shipstation.php
@@ -15,7 +15,7 @@ require_once 'app/Mage.php';
 Mage::app();
 umask(0);
 
-$date_to_look_start = date('Y-m-d', strtotime('-1 day', strtotime(date('Y-m-d'))));
+$date_to_look_start = date('Y-m-d', strtotime('-2 day', strtotime(date('Y-m-d'))));
 $date_to_look_end = date('Y-m-d', strtotime(date('Y-m-d')));
 
 
@@ -24,10 +24,14 @@ $orders = Mage::getModel('sales/order')
     ->addAttributeToFilter('created_at', array('gteq' => $date_to_look_start . ' 00:00:01'))
     ->addAttributeToFilter('created_at', array('lteq' => $date_to_look_end . ' 23:59:59'));
 
-echo '<pre/>';
-//print_r($orders->getData());
+header('Content-Type: text/csv; charset=utf-8');
+header('Content-Disposition: attachment; filename=orderdata4shipstation.csv');
+$fp = fopen('php://output', 'w');
 
-
+fputcsv($fp, array("order_id","created_at","grand_total","total_paid","tax_amount","shipping_amount","shipping_description","customer name","customer_firstname","customer_lastname","customer_email",
+    "shipping_telephone","shipping_first_name","shipping_last_name","shipping_full_name","shipping_address","shipping_city","shipping_region","shipping_postcode","shipping_country",
+    "billing_telephone","billing_first_name","billing_last_name","billing_full_name","billing_address","billing_city","billing_region","billing_postcode","billing_country",
+    "product_name","sku","qty","price"));
 
 $items =array();
 foreach($orders as $order){
@@ -82,8 +86,10 @@ if($order->getIncrementId()=='100021922') {
         $items[] = round($item->getQtyOrdered());
         $items[] = round($item->getPrice());
     }
+    fputcsv($fp,$items);
 }
 
 }
-echo '<pre/>';
-print_r($items);
+
+fclose($fp);
+exit;
