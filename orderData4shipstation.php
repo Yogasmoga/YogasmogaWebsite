36007c2db0cf34abfe38c72bfaf7979570f62edd
info@yogasmoga.com <info@yogasmoga.com>
2016-11-24 20:14:10 +0530

    ship

--- orderData4shipstation.php
+++ orderData4shipstation.php
@@ -15,7 +15,7 @@ require_once 'app/Mage.php';
 Mage::app();
 umask(0);
 
-$date_to_look_start = date('Y-m-d', strtotime('-1 day', strtotime(date('Y-m-d'))));
+$date_to_look_start = date('Y-m-d', strtotime('-2 day', strtotime(date('Y-m-d'))));
 $date_to_look_end = date('Y-m-d', strtotime(date('Y-m-d')));
 
 
@@ -28,57 +28,60 @@ header('Content-Type: text/csv; charset=utf-8');
 header('Content-Disposition: attachment; filename=orderdata4shipstation.csv');
 $fp = fopen('php://output', 'w');
 
-fputcsv($fp, array("order_id","created_at","grand_total","total_paid","tax_amount","shipping_amount","shipping_description","customer name","customer_firstname","customer_lastname","customer_email",
-    "shipping_telephone","shipping_first_name","shipping_last_name","shipping_full_name","shipping_address","shipping_city","shipping_region","shipping_postcode","shipping_country",
-    "billing_telephone","billing_first_name","billing_last_name","billing_full_name","billing_address","billing_city","billing_region","billing_postcode","billing_country",
+fputcsv($fp, array("order_id","created_at","grand_total","total_paid","tax_amount","shipping_amount",
+    "shipping_description","customer_name","customer_firstname","customer_lastname",
+    "customer_email","shipping_telephone","shipping_first_name","shipping_last_name",
+    "shipping_full_name","shipping_address","shipping_city","shipping_region","shipping_postcode",
+    "shipping_country","billing_telephone","billing_first_name","billing_last_name","billing_full_name",
+    "billing_address","billing_city","billing_region","billing_postcode","billing_country",
     "product_name","sku","qty","price"));
 
-$items =array();
+
 foreach($orders as $order) {
 //$orderId = '100021922';
 //if($order->getIncrementId()=='100021922') {
 
     $order = Mage::getModel('sales/order')->loadByIncrementId($order->getIncrementId());
-    //print_r($order->getData());
-    //echo "<hr/>";
+
 
 
     /****************--------------***********/
-    $items[] = $order->getData('increment_id');
-    $items[] = $order->getData('created_at');
-    $items[] = $order->getData('grand_total');
-    $items[] = $order->getData('total_paid');
-    $items[] = $order->getData('tax_amount');
-    $items[] = $order->getData('shipping_amount');
-    $items[] = $order->getData('shipping_description');
-    $items[] = $order->getData('customer_firstname') . ' ' . $order->getData('customer_lastname');
-    $items[] = $order->getData('customer_firstname');
-    $items[] = $order->getData('customer_lastname');
-    $items[] = $order->getData('customer_email');
+    $order_id = $order->getData('increment_id');
+    $created_at = $order->getData('created_at');
+    $grand_total = $order->getData('grand_total');
+    $total_paid = $order->getData('total_paid');
+    $tax_amount = $order->getData('tax_amount');
+    $shipping_amount = $order->getData('shipping_amount');
+    $shipping_description = $order->getData('shipping_description');
+    $name = $order->getData('customer_firstname') . ' ' . $order->getData('customer_lastname');
+    $firstname = $order->getData('customer_firstname');
+    $lastname = $order->getData('customer_lastname');
+    $email = $order->getData('customer_email');
 
     //echo "Shipping Address.<br/>";
 
-    $items[] = $order->getShippingAddress()->getTelephone();
-    $items[] = $order->getShippingAddress()->getFirstname();
-    $items[] = $order->getShippingAddress()->getLastname();
-    $items[] = $order->getShippingAddress()->getFirstname() . ' ' . $order->getShippingAddress()->getLastname();
-    $items[] = $order->getShippingAddress()->getData('street');
-    $items[] = $order->getShippingAddress()->getCity();
-    $items[] = $order->getShippingAddress()->getRegion();
-    $items[] = $order->getShippingAddress()->getPostcode();
-    $items[] = $order->getShippingAddress()->getCountryId();
+    $stelephone = $order->getShippingAddress()->getTelephone();
+    $sfirstname = $order->getShippingAddress()->getFirstname();
+    $slastname = $order->getShippingAddress()->getLastname();
+    $sfullname = $order->getShippingAddress()->getFirstname() . ' ' . $order->getShippingAddress()->getLastname();
+    $saddress = $order->getShippingAddress()->getData('street');
+    $scity = $order->getShippingAddress()->getCity();
+    $sregion = $order->getShippingAddress()->getRegion();
+    $spostcode = $order->getShippingAddress()->getPostcode();
+    $scountry = $order->getShippingAddress()->getCountryId();
 
     //echo "Billing Address <br/>";
-    $items[] = $order->getBillingAddress()->getTelephone();
-    $items[] = $order->getBillingAddress()->getFirstname();
-    $items[] = $order->getBillingAddress()->getLastname();
-    $items[] = $order->getBillingAddress()->getFirstname() . ' ' . $order->getShippingAddress()->getLastname();
-    $items[] = $order->getBillingAddress()->getData('street');
-    $items[] = $order->getBillingAddress()->getCity();
-    $items[] = $order->getBillingAddress()->getRegion();
-    $items[] = $order->getBillingAddress()->getPostcode();
-    $items[] = $order->getBillingAddress()->getCountryId();
+    $btelephone = $order->getBillingAddress()->getTelephone();
+    $bfirstname = $order->getBillingAddress()->getFirstname();
+    $lastname = $order->getBillingAddress()->getLastname();
+    $bfullname = $order->getBillingAddress()->getFirstname() . ' ' . $order->getShippingAddress()->getLastname();
+    $bstreet = $order->getBillingAddress()->getData('street');
+    $bcity = $order->getBillingAddress()->getCity();
+    $bregion = $order->getBillingAddress()->getRegion();
+    $bpostcode = $order->getBillingAddress()->getPostcode();
+    $bcountry = $order->getBillingAddress()->getCountryId();
 
+    $items=array();
     foreach ($order->getAllVisibleItems() as $item) {
 
         $items[] = $item->getName();
@@ -86,7 +89,13 @@ foreach($orders as $order) {
         $items[] = $item->getQtyOrdered();
         $items[] = $item->getPrice();
     }
-    fputcsv($fp, $items);
+
+    $arr1 =   array($order_id,$created_at,$grand_total,$total_paid,$tax_amount,$shipping_amount,$shipping_description,$name,$firstname,$lastname,$email,
+        $stelephone,$sfirstname,$slastname,$sfullname,$saddress,$scity,$sregion,$spostcode,$scountry,$btelephone,$bfirstname,
+        $lastname,$bfullname,$bstreet,$bcity,$bregion,$bpostcode,$bcountry);
+    $finalData = array_merge($arr1,$items);
+
+    fputcsv($fp,$finalData );
 
 //}
 }
