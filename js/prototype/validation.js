3781d00005195dd3d3d8ce88983aac6036f43bc3
Fahim khan <info@yogasmoga.com>
2017-01-10 19:33:16 +0530

    zip code update

--- js/prototype/validation.js
+++ js/prototype/validation.js
@@ -564,9 +564,13 @@ Validation.addAllThese([
             return Validation.get('IsEmpty').test(v) || /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(v);
             }],
     ['validate-zip-international', 'Please enter a valid zip code.', function(v) {
-            return Validation.get('IsEmpty').test(v) || /(^\d{4,10}$)|(^\d{5}-\d{4}$)/.test(v);
+            //return Validation.get('IsEmpty').test(v) || /(^\d{4,10}$)|(^\d{5}-\d{4}$)/.test(v);
             //return Validation.get('IsEmpty').test(v) || /(^[A-z0-9]{2,10}([\s]{0,1}|[\-]{0,1})[A-z0-9]{2,10}$)/.test(v);
             //return true;
+			
+			return Validation.get('IsEmpty').test(v) || /(^\d{5}(-\d{4})?$)|(^[ABCEGHJKLMNPRSTVXY]{1}\d{1}[A-Z]{1} *\d{1}[A-Z]{1}\d{1}$)/.test(v);
+            return true;
+			
             }],
     ['validate-date-au', 'Please use this date format: dd/mm/yyyy. For example 17/03/2006 for the 17th of March, 2006.', function(v) {
                 if(Validation.get('IsEmpty').test(v)) return true;
