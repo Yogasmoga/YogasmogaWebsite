9aea9ae70eb3e1730c3f432e8c59afac38172c85
Fahim khan <info@yogasmoga.com>
2017-02-07 20:04:38 +0530

    js update

--- js/ys-new-design/myproductdetails.js
+++ js/ys-new-design/myproductdetails.js
@@ -584,16 +584,16 @@ function changeproductsize(sz) {
 
 					if (sz.hasClass("showing-pre")) {
 						alert("15");
-						alert(jQuery(".showing-pre").attr("showpremsg"));
-						//var preordermsg =  sz.attr("showpremsg");
+						//alert(jQuery(".showing-pre").attr("showpremsg"));
+						var preordermsg =  sz.attr("showpremsg");
 						
-						//premsghtml = premsghtml + preordermsg;
-						//jQuery(".ship-msg").html(preordermsg);
+						premsghtml = premsghtml + preordermsg;
+						jQuery(".ship-msg").html(preordermsg);
 						//alert(""+premsghtml);
 						jQuery("#orderitem").hide();
 						jQuery("#preorderitem").show();
-						//jQuery(".ship-msg").show();
-						//jQuery("#preorderhelp").show();
+						jQuery(".ship-msg").show();
+						jQuery("#preorderhelp").show();
 						jQuery("#outofstockitem").hide();
 					}
 					else{
