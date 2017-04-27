4ca4278f3cf53de59057011fcad162a04d3067db
Fahim khan <info@yogasmoga.com>
2017-04-27 18:30:28 +0530

    our store module created

--- js/ys-new-design/homemainsection.js
+++ js/ys-new-design/homemainsection.js
@@ -9,7 +9,7 @@ jQuery(document).ready(function($){
 //    var bannerFliudHeight = ww * 0.3125;
     $(".gridfull").css('min-height',ww*0.48);
     $("#bucket1_slider.flexslider .slides li").css({"height": sliderHeight});
-	jQuery(".slider-block .slides .slider_li").height(360);
+	jQuery(".slider-block .slides .slider_li").height(510);
 
     //$(flexslider)
     $('.flexslider').flexslider({
@@ -73,7 +73,7 @@ jQuery(document).ready(function($){
         //jQuery(".gridfull").css('min-height',jQuery(window).width()*0.48);
         //jQuery("#bucket1_slider.flexslider .slides li").css({"height": sliderHeight});
         jQuery(".gridfull.nophotoshop .slider_li").css('min-height',ww*0.48);
-		jQuery(".slider-block .slides .slider_li").height(360);
+		jQuery(".slider-block .slides .slider_li").height(500);
         //alert("resized");
         // positionfloatingimages();
         
